6d3ec9ee64b9b67b8f38f36aa632efc5abdfe637
lladdy <randallboland@bigpond.com>
2019-03-17 09:54:42 +1030

    Add more models for basic ladder functionality

--- aiarena/core/admin.py
+++ aiarena/core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Bot
+from .models import *
 
+admin.site.register(Sc2Race)
 admin.site.register(Bot)
+admin.site.register(Map)
+admin.site.register(ResultType)
+admin.site.register(Result)
--- aiarena/core/models.py
+++ aiarena/core/models.py
@@ -2,8 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Sc2Race(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
 class Bot(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
     active = models.BooleanField(default=False)
     elo = models.SmallIntegerField(default=1600)
+    bot_zip = models.FileField()
+    bot_zip_md5hash = models.CharField(max_length=50)
+    plays_race = models.ForeignKey(Sc2Race, on_delete=models.PROTECT)
+
+
+class Map(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    file = models.FileField()
+
+
+class ResultType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Result(models.Model):
+    bot1 = models.ForeignKey(Bot, on_delete=models.PROTECT, related_name='bot1')
+    bot2 = models.ForeignKey(Bot, on_delete=models.PROTECT, related_name='bot2')
+    winner = models.ForeignKey(Bot, on_delete=models.PROTECT, related_name='winner')
+    result_type = models.ForeignKey(ResultType, on_delete=models.PROTECT)
+    map = models.ForeignKey(Map, on_delete=models.PROTECT)
+    datetime = models.DateTimeField()
