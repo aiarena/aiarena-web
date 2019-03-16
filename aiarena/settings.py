0d2803f4cb9f67639f328008e5ada17e69daf26f
lladdy <randallboland@bigpond.com>
2019-03-16 14:13:14 +1030

    User registration working with email

--- aiarena/settings.py
+++ aiarena/settings.py
@@ -65,26 +65,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aiarena.wsgi.application'
 
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
