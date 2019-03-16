1d796ad43c3e7206877e61d481da4edc6b341074
lladdy <randallboland@bigpond.com>
2019-03-16 13:11:04 +1030

    Configure django-registration urls

--- aiarena/urls.py
+++ aiarena/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/', include('django_registration.backends.activation.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
