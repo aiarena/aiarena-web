c68624f4d9ea0eaee95e6141a58c851026554619
Florian Ludee <florian.luettgens@gmail.com>
2019-03-16 07:37:43 +0000

    Enables MySQL strict mode

--- aiarena/env-example.py
+++ aiarena/env-example.py
@@ -22,5 +22,8 @@ DATABASES = {
         'PASSWORD': 'aiarena',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
