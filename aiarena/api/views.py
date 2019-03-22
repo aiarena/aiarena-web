8a301922d2397efcd6b85977afe5dc1f547453e1
lladdy <randallboland@bigpond.com>
2019-03-23 09:42:50 +1030

    Expose models in API

--- aiarena/api/views.py
+++ aiarena/api/views.py
@@ -1,4 +1,4 @@
-from aiarena.core.models import Bot
+from aiarena.core.models import Bot, Map, Match, Participant, Result
 from rest_framework import viewsets, serializers
 
 # todo: restrict aiarena-client specific endpoints to staff only
@@ -12,3 +12,47 @@ class BotSerializer(serializers.ModelSerializer):
 class BotViewSet(viewsets.ModelViewSet):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
+
+
+class MapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Map
+        fields = '__all__'
+
+
+class MapViewSet(viewsets.ModelViewSet):
+    queryset = Map.objects.all()
+    serializer_class = MapSerializer
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = '__all__'
+
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = '__all__'
+
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = '__all__'
+
+
+class ResultViewSet(viewsets.ModelViewSet):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
