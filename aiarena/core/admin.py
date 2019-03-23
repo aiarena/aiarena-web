e58fc022e54dbb3d45045f2e70b2c0fe7297c6ef
lladdy <randallboland@bigpond.com>
2019-03-23 16:45:13 +1030

    Add models to admin

--- aiarena/core/admin.py
+++ aiarena/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(User)
 admin.site.register(Bot)
 admin.site.register(Map)
+admin.site.register(Match)
+admin.site.register(Participant)
 admin.site.register(Result)
