d7ca29085d1d000131cb8dc35c882df3eaa01543
lladdy <randallboland@bigpond.com>
2019-03-30 23:43:57 +1030

    Add commented out failing test.

--- aiarena/core/tests.py
+++ aiarena/core/tests.py
@@ -1,8 +1,9 @@
 import os
 
+from django.core.files import File
 from django.test import TestCase
 
-from aiarena.core.models import User
+from aiarena.core.models import User, Bot
 from aiarena.core.utils import calculate_md5
 
 
@@ -16,3 +17,13 @@ class UtilsTestCase(TestCase):
 class UserTestCase(TestCase):
     def test_user_creation(self):
         User.objects.create(username='test user', email='test@test.com')
+
+# This should work, but I get the error:
+# OSError: [WinError 123] The filename, directory name, or volume label syntax is incorrect
+# class BotTestCase(TestCase):
+#     def test_bot_creation(self):
+#         user = User.objects.create(username='test user', email='test@test.com')
+#         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_bot.zip')
+#         file = open(filename, 'rb')
+#         Bot.objects.create(user=user, name='test', bot_zip=File(file), plays_race='T', type='Python')
+
