3d8dfaa260557d2e96cec230cc75d5a61ee9eed9
lladdy <randallboland@bigpond.com>
2019-03-30 23:36:19 +1030

    Add test for calculate_md5

--- aiarena/core/tests.py
+++ aiarena/core/tests.py
@@ -1,6 +1,16 @@
+import os
+
 from django.test import TestCase
 
-from aiarena.core.models import *
+from aiarena.core.models import User
+from aiarena.core.utils import calculate_md5
+
+
+class UtilsTestCase(TestCase):
+    def test_calc_md5(self):
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_bot.zip')
+        file = open(filename, 'rb')
+        self.assertEqual('7411028ba931baaad47bf5810215e4f8', calculate_md5(file))
 
 
 class UserTestCase(TestCase):
