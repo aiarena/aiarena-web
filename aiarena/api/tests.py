84e80f7dc1122781456f45573f1144a086608510
lladdy <randallboland@bigpond.com>
2019-03-30 19:00:36 +1030

    Consolidate match generation tests.

--- aiarena/api/tests.py
+++ aiarena/api/tests.py
@@ -5,16 +5,40 @@ from aiarena.core.models import *
 
 class AuthorisedMatchTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username='test', password='x', is_superuser=True)
+        self.testUser = User.objects.create_user(username='test', password='x', is_superuser=True)
         self.client.login(username='test', password='x')
 
-    def test_next_match_no_maps(self):
-        # not having any maps should cause this to fall over
+    def test_next_match(self):
+        # no maps
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
 
-    def test_next_match_no_bots(self):
+        # not enough active bots
         Map.objects.create(name='testmap')
-        # not having any bots should cause this to fall over
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
+
+        # not enough active bots
+        bot1 = Bot.objects.create(user=self.testUser, name='testbot1')
+        response = self.client.get('/api/matches/next/')
+        self.assertEqual(response.status_code, 500)
+
+        # not enough active bots
+        bot2 = Bot.objects.create(user=self.testUser, name='testbot2')
+        response = self.client.get('/api/matches/next/')
+        self.assertEqual(response.status_code, 500)
+
+        # not enough active bots
+        bot1.active = True
+        bot1.save()
+        response = self.client.get('/api/matches/next/')
+        self.assertEqual(response.status_code, 500)
+
+        # success
+        bot2.active = True
+        bot2.save()
+        response = self.client.get('/api/matches/next/')
+        self.assertEqual(response.status_code, 200)
+
+        # ensure only 1 match was created
+        self.assertEqual(Match.objects.count(), 1)
