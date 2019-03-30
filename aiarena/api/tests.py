fa27e385c3951bcb1638fa79991272f27f61dafb
lladdy <randallboland@bigpond.com>
2019-03-31 00:35:00 +1030

    Fix all broken tests.

--- aiarena/api/tests.py
+++ aiarena/api/tests.py
@@ -1,5 +1,6 @@
 import os
 
+from django.core.files import File
 from django.test import TestCase
 
 from aiarena.core.models import *
@@ -26,6 +27,10 @@ class MatchesTestCase(LoggedInTestCase):
     def test_next_match(self):
         self.client.login(username='staff_user', password='x')
 
+        # For some reason using an absolute file path here for will cause it to mangle the save directory and fail
+        # later whilst handling the bot_zip file save
+        bot_zip = open('./aiarena/api/test_bot.zip', 'rb')
+
         # no maps
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
@@ -36,12 +41,12 @@ class MatchesTestCase(LoggedInTestCase):
         self.assertEqual(response.status_code, 500)
 
         # not enough active bots
-        bot1 = Bot.objects.create(user=self.staffUser, name='testbot1')
+        bot1 = Bot.objects.create(user=self.staffUser, name='testbot1', bot_zip=File(bot_zip))
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
 
         # not enough active bots
-        bot2 = Bot.objects.create(user=self.regularUser, name='testbot2')
+        bot2 = Bot.objects.create(user=self.regularUser, name='testbot2', bot_zip=File(bot_zip))
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
 
@@ -72,8 +77,12 @@ class ResultsTestCase(LoggedInTestCase):
 
     def CreateMatch(self):
         Map.objects.create(name='testmap')
-        bot1 = Bot.objects.create(user=self.regularUser, name='testbot1', active=True)
-        bot2 = Bot.objects.create(user=self.staffUser, name='testbot2', active=True)
+
+        # For some reason using an absolute file path here for will cause it to mangle the save directory and fail
+        # later whilst handling the bot_zip file save
+        bot_zip = open('./aiarena/api/test_bot.zip', 'rb')
+        bot1 = Bot.objects.create(user=self.regularUser, name='testbot1', bot_zip=File(bot_zip), active=True)
+        bot2 = Bot.objects.create(user=self.staffUser, name='testbot2', bot_zip=File(bot_zip), active=True)
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 200)
         return response.data, bot1, bot2
@@ -82,10 +91,10 @@ class ResultsTestCase(LoggedInTestCase):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testReplay.SC2Replay')
         with open(filename) as replayFile:
             return self.client.post('/api/results/',
-                                        {'match': match["id"],
-                                         'winner': winner.id,
-                                         'type': 'P1W',
-                                         'replay_file': replayFile})
+                                    {'match': match["id"],
+                                     'winner': winner.id,
+                                     'type': 'P1W',
+                                     'replay_file': replayFile})
 
     def test_create_result(self):
         self.client.login(username='staff_user', password='x')
--- aiarena/core/tests.py
+++ aiarena/core/tests.py
@@ -18,12 +18,12 @@ class UserTestCase(TestCase):
     def test_user_creation(self):
         User.objects.create(username='test user', email='test@test.com')
 
-# This should work, but I get the error:
-# OSError: [WinError 123] The filename, directory name, or volume label syntax is incorrect
-# class BotTestCase(TestCase):
-#     def test_bot_creation(self):
-#         user = User.objects.create(username='test user', email='test@test.com')
-#         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_bot.zip')
-#         file = open(filename, 'rb')
-#         Bot.objects.create(user=user, name='test', bot_zip=File(file), plays_race='T', type='Python')
 
+class BotTestCase(TestCase):
+    def test_bot_creation(self):
+        user = User.objects.create(username='test user', email='test@test.com')
+        # For some reason using an absolute file path here for will cause it to mangle the save directory and fail
+        # later whilst handling the bot_zip file save
+        file = open('./aiarena/core/test_bot.zip', 'rb')
+        bot = Bot.objects.create(user=user, name='test', bot_zip=File(file), plays_race='T', type='Python')
+        self.assertEqual('7411028ba931baaad47bf5810215e4f8', bot.bot_zip_md5hash)
