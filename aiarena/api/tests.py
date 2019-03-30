fdef5e60c3a6836fbb15365255a790e4019a18d0
lladdy <randallboland@bigpond.com>
2019-03-30 20:27:53 +1030

    Add API create result tests

--- aiarena/api/tests.py
+++ aiarena/api/tests.py
@@ -1,14 +1,19 @@
+import os
+
 from django.test import TestCase
 
 from aiarena.core.models import *
 
 
-class MatchesTestCase(TestCase):
+class LoggedInTestCase(TestCase):
     def setUp(self):
         self.staffUser = User.objects.create_user(username='staff_user', password='x', email='staff_user@aiarena.net',
                                                   is_staff=True)
         self.regularUser = User.objects.create_user(username='regular_user', password='x',
                                                     email='regular_user@aiarena.net')
+
+
+class MatchesTestCase(LoggedInTestCase):
 
     def test_next_match_not_authorized(self):
         response = self.client.get('/api/matches/next/')
@@ -18,7 +23,7 @@ class MatchesTestCase(TestCase):
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 403)
 
-    def test_next_match_staff_user(self):
+    def test_next_match(self):
         self.client.login(username='staff_user', password='x')
 
         # no maps
@@ -36,7 +41,7 @@ class MatchesTestCase(TestCase):
         self.assertEqual(response.status_code, 500)
 
         # not enough active bots
-        bot2 = Bot.objects.create(user=self.staffUser, name='testbot2')
+        bot2 = Bot.objects.create(user=self.regularUser, name='testbot2')
         response = self.client.get('/api/matches/next/')
         self.assertEqual(response.status_code, 500)
 
@@ -54,3 +59,38 @@ class MatchesTestCase(TestCase):
 
         # ensure only 1 match was created
         self.assertEqual(Match.objects.count(), 1)
+
+
+class ResultsTestCase(LoggedInTestCase):
+    def test_create_result_not_authorized(self):
+        response = self.client.get('/api/results/')
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(username='regular_user', password='x')
+        response = self.client.get('/api/results/')
+        self.assertEqual(response.status_code, 403)
+
+    def CreateMatch(self):
+        Map.objects.create(name='testmap')
+        bot1 = Bot.objects.create(user=self.regularUser, name='testbot1', active=True)
+        bot2 = Bot.objects.create(user=self.staffUser, name='testbot2', active=True)
+        response = self.client.get('/api/matches/next/')
+        self.assertEqual(response.status_code, 200)
+        return response.data, bot1, bot2
+
+    def PostResult(self, match, winner):
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testReplay.SC2Replay')
+        with open(filename) as replayFile:
+            return self.client.post('/api/results/',
+                                        {'match': match["id"],
+                                         'winner': winner.id,
+                                         'type': 'P1W',
+                                         'replay_file': replayFile})
+
+    def test_create_result(self):
+        self.client.login(username='staff_user', password='x')
+
+        match, bot1, bot2 = self.CreateMatch()
+
+        response = self.PostResult(match, bot1)
+        self.assertEqual(response.status_code, 201)
