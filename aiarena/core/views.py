9720e5449dc88ccf4aa1385c9bcbbabd4d72a1af
lladdy <randallboland@bigpond.com>
2019-04-02 01:25:07 +1030

    Add bot upload form.

--- aiarena/core/views.py
+++ aiarena/core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from aiarena.core.models import Bot
+
+
+class BotUpload(SuccessMessageMixin, LoginRequiredMixin, CreateView):
+    redirect_field_name = 'next'
+    template_name = 'botupload.html'
+    success_message = "Bot was uploaded successfully"
+
+    model = Bot
+    fields = ['name', 'bot_zip', 'plays_race', 'type']
+
+    def get_login_url(self):
+        return reverse('login')
+
+    def get_success_url(self):
+        return reverse('botupload')
+
+    def get_form_kwargs(self):
+        # set the bot's user
+        kwargs = super(BotUpload, self).get_form_kwargs()
+        if kwargs['instance'] is None:
+            kwargs['instance'] = Bot()
+        kwargs['instance'].user = self.request.user
+        return kwargs
--- aiarena/urls.py
+++ aiarena/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
+
+from aiarena.core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,5 @@ urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     path('api/', include('aiarena.api.urls')),
-    # url(r'^api-auth/', include('rest_framework.urls')), # if we want to use the DRF login/out forms
+    path('botupload/', core_views.BotUpload.as_view(), name='botupload'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # https://stackoverflow.com/questions/5517950/django-media-url-and-media-root
