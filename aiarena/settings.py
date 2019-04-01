40cefb1d0d07983b177f79a4fd47a33e26ac777d
lladdy <randallboland@bigpond.com>
2019-04-01 15:51:10 +1030

    Remove unrequired installed app dependency

--- aiarena/settings.py
+++ aiarena/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_registration',
     'rest_framework',
     'rest_framework.authtoken',
     'aiarena.core',
