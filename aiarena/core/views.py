95e6bbd477dd5f4d66e197078d36c90f2e399cc2
lladdy <randallboland@bigpond.com>
2019-04-28 16:38:41 +0930

    Convert ranking to use build in Django ListView and display username.

--- aiarena/core/views.py
+++ aiarena/core/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.shortcuts import render
 from aiarena.core.models import Bot
 from aiarena.core.models import Result
@@ -30,11 +30,24 @@ class BotUpload(SuccessMessageMixin, LoginRequiredMixin, CreateView):
         return kwargs
 
 
-class Ranking(CreateView):
-    def get(self, request):
-        bot_ranking = Bot.objects.filter(active=1).order_by('-elo')
-        context = {'bot_ranking': bot_ranking}
-        return render(request, 'ranking.html', context)
+class Ranking(ListView):
+    # If we wanted all the bots, we could just set this and, because we're extending a ListView, it would
+    # understand to get all the Bots, which could be referenced as "bot_list" in the template.
+    # model = Bot
+
+    # Instead, because we want to filter the bots, we define queryset,
+    # which is also referenced as "bot_list" in the template.
+    queryset = Bot.objects.filter(active=1).order_by('-elo')
+
+    # If we didn't set this, the ListView would default to searching for a template at <module>/<modelname>_list.html
+    # In this case, that would be "core/bot_list.html"
+    template_name = 'ranking.html'
+
+    # Old code for reference.
+    # def get(self, request):
+    #     bot_ranking = Bot.objects.filter(active=1).order_by('-elo')
+    #     context = {'bot_ranking': bot_ranking}
+    #     return render(request, 'ranking.html', context)
 
 
 class Results(CreateView):
