d7c5eab618aa0a0296e941cf670475e9cceae727
lladdy <randallboland@bigpond.com>
2019-05-02 22:41:42 +0930

    Clarify stats labelling and add more stats.

--- aiarena/core/context_processors.py
+++ aiarena/core/context_processors.py
@@ -5,6 +5,7 @@ from aiarena.core.models import Match, Bot
 
 def stats(request):
     return {
-        'matches_count': Match.objects.filter(created__gte=datetime.now() - timedelta(hours=24)).count(),
+        'match_count_1h': Match.objects.filter(created__gte=datetime.now() - timedelta(hours=1)).count(),
+        'match_count_24h': Match.objects.filter(created__gte=datetime.now() - timedelta(hours=24)).count(),
         'active_bots': Bot.objects.filter(active=True).count(),
     }
