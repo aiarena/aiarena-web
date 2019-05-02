544ef2f5a464ffadfb09152f62e451ce7528b8da
lladdy <randallboland@bigpond.com>
2019-05-02 23:06:46 +0930

    Fix runtime warning regarding use of timezone naive datetimes

--- aiarena/core/context_processors.py
+++ aiarena/core/context_processors.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
+from django.utils import timezone
 
 from aiarena.core.models import Match, Bot
 
 
 def stats(request):
     return {
-        'match_count_1h': Match.objects.filter(created__gte=datetime.now() - timedelta(hours=1)).count(),
-        'match_count_24h': Match.objects.filter(created__gte=datetime.now() - timedelta(hours=24)).count(),
+        'match_count_1h': Match.objects.filter(created__gte=timezone.now() - timedelta(hours=1)).count(),
+        'match_count_24h': Match.objects.filter(created__gte=timezone.now() - timedelta(hours=24)).count(),
         'active_bots': Bot.objects.filter(active=True).count(),
     }
