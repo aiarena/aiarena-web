6b94f14d68e761936258909c430ef46fdc3db1b6
lladdy <randallboland@bigpond.com>
2019-05-31 04:06:18 +0930

    Fix bot page render

--- aiarena/core/templatetags/core_filters.py
+++ aiarena/core/templatetags/core_filters.py
@@ -1,8 +1,13 @@
 from django import template
 
+
 def with_symbol(value):
     """Formats an integer so as to include the positive or negative symbol"""
-    return "%+d" % value
+    if value is None:
+        return ""
+    else:
+        return "%+d" % value
+
 
 register = template.Library()
 register.filter('with_symbol', with_symbol)
