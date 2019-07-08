8da70e84110d1424438a86817eaa4d524606ddb0
lladdy <randallboland@bigpond.com>
2019-07-09 04:54:36 +0930

    Use results to calculate match stats

--- aiarena/core/context_processors.py
+++ aiarena/core/context_processors.py
@@ -2,13 +2,13 @@ from datetime import timedelta
 
 from django.utils import timezone
 
-from aiarena.core.models import Match, Bot
+from aiarena.core.models import Bot, Result
 
 
 # these are available globally in the django templates
 def stats(request):
     return {
-        'match_count_1h': Match.objects.filter(created__gte=timezone.now() - timedelta(hours=1)).count(),
-        'match_count_24h': Match.objects.filter(created__gte=timezone.now() - timedelta(hours=24)).count(),
+        'match_count_1h': Result.objects.filter(created__gte=timezone.now() - timedelta(hours=1)).count(),
+        'match_count_24h': Result.objects.filter(created__gte=timezone.now() - timedelta(hours=24)).count(),
         'active_bots': Bot.objects.filter(active=True).count(),
     }
