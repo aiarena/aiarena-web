cbcc2791816654d4adef349c625c15a53db57914
lladdy <randallboland@bigpond.com>
2019-07-09 06:39:09 +0930

    Refine cancelmatches management command to better handle updates and use transactions

--- aiarena/core/management/commands/cancelmatches.py
+++ aiarena/core/management/commands/cancelmatches.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 
 from aiarena.core.exceptions import BotNotInMatchException
 from aiarena.core.models import Match, Result
@@ -11,30 +12,34 @@ class Command(BaseCommand):
         parser.add_argument('match_ids', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        for match_id in options['match_ids']:
-            try:
-                match = Match.objects.get(pk=match_id)
-            except Match.DoesNotExist:
-                raise CommandError('Match "%s" does not exist' % match_id)
+        with transaction.atomic():
+            for match_id in options['match_ids']:
+                try:
+                    match = Match.objects.select_for_update().get(pk=match_id)
+                except Match.DoesNotExist:
+                    raise CommandError('Match "%s" does not exist' % match_id)
 
-            if Result.objects.filter(match=match).count() > 0:
-                raise CommandError('A result already exists for match "%s"' % match_id)
+                if Result.objects.filter(match=match).count() > 0:
+                    raise CommandError('A result already exists for match "%s"' % match_id)
 
-            Result.objects.create(match=match, type='InitializationError', duration=0)
+                Result.objects.create(match=match, type='InitializationError', duration=0)
 
-            # attempt to kick the bots from the match
-            bot1 = match.participant_set.get(participant_number=1).bot
-            bot2 = match.participant_set.get(participant_number=2).bot
-            try:
-                bot1.leave_match(match_id)
-            except BotNotInMatchException:
+                # attempt to kick the bots from the match
+                if match.started:
+                    try:
+                        bot1 = match.participant_set.select_related().select_for_update().get(participant_number=1).bot
+                        bot1.leave_match(match_id)
+                    except BotNotInMatchException:
+                        self.stdout.write(
+                            'WARNING! Match "{1}": Participant 1 bot "{0}" was not registered as in this match, despite the match having started.'.format(
+                                bot1.id, match_id))
+                    try:
+                        bot2 = match.participant_set.select_related().select_for_update().get(participant_number=2).bot
+                        bot2.leave_match(match_id)
+                    except BotNotInMatchException:
+                        self.stdout.write(
+                            'WARNING! Match "{1}": Participant 2 bot "{0}" was not registered as in this match, despite the match having started.'.format(
+                                bot1.id, match_id))
+
                 self.stdout.write(
-                    'WARNING! Match "{1}": Participant 1 bot "{0}" was not registered as in this match.'.format(bot1.id, match_id))
-            try:
-                bot2.leave_match(match_id)
-            except BotNotInMatchException:
-                self.stdout.write(
-                    'WARNING! Match "{1}": Participant 2 bot "{0}" was not registered as in this match.'.format(bot1.id, match_id))
-
-            self.stdout.write(
-                self.style.SUCCESS('Successfully marked match "%s" with an InitializationError' % match_id))
+                    self.style.SUCCESS('Successfully marked match "%s" with an InitializationError' % match_id))
