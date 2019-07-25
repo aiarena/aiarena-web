e1f564c9bf40c928d28b7768a161a02e9522a355
lladdy <randallboland@bigpond.com>
2019-07-26 06:29:42 +0930

    Make generate_stats updateable

---
+++ aiarena/scripts.py
@@ -0,0 +1,11 @@
+import os
+
+from aiarena.settings import SCRIPTS_ROOT
+from django.db import connection
+
+
+def run_usp_generate_stats_sql():
+    file_path = os.path.join(SCRIPTS_ROOT, 'usp_generate_stats.sql')
+    sql_statement = open(file_path).read()
+    with connection.cursor() as c:
+        c.execute(sql_statement)
