2a07146db3e744af50828f120727cb79f34f8ae3
lladdy <randallboland@bigpond.com>
2019-08-16 11:25:41 +0930

    Fix generate_stats not being created or updated properly

--- aiarena/scripts.py
+++ aiarena/scripts.py
@@ -8,4 +8,7 @@ def run_usp_generate_stats_sql():
     file_path = os.path.join(SCRIPTS_ROOT, 'usp_generate_stats.sql')
     sql_statement = open(file_path).read()
     with connection.cursor() as c:
-        c.executemany(sql_statement, [])
+        # Drop the stored procedure if it already exists
+        c.execute("DROP PROCEDURE IF EXISTS `generate_stats`;")
+        # Then create it...
+        c.execute(sql_statement)
