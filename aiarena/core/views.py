3acc775cdc5320651074c8a9bdaaacb63937cb47
lladdy <randallboland@bigpond.com>
2019-10-18 20:33:19 +1030

    autoformat

--- aiarena/core/views.py
+++ aiarena/core/views.py
@@ -188,7 +188,6 @@ class BotUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     def get_form(self, form_class=None):
         """Return an instance of the form to be used in this view."""
 
-
         if form_class is None:
             form_class = self.get_form_class()
 
