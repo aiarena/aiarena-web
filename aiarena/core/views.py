e1e82d9a97022187aecdf9f134b7c4d5ebf9b246
lladdy <randallboland@bigpond.com>
2019-10-18 20:32:55 +1030

    Fix bot article content not loading.

--- aiarena/core/views.py
+++ aiarena/core/views.py
@@ -185,6 +185,19 @@ class BotUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         else:
             return StandardBotUpdateForm
 
+    def get_form(self, form_class=None):
+        """Return an instance of the form to be used in this view."""
+
+
+        if form_class is None:
+            form_class = self.get_form_class()
+
+        # load the wiki article content as the initial value
+        wiki_article_content = self.object.wiki_article.current_revision.content
+        kwargs = self.get_form_kwargs()
+        kwargs['initial']['wiki_article_content'] = wiki_article_content
+        return form_class(**kwargs)
+
     def form_valid(self, form):
         """Create a new article revision for the bot wiki page when the form is valid"""
 
