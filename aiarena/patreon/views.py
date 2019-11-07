bee0a7cea492f5a56035f8417dbc5b0eefd4c3df
lladdy <randallboland@bigpond.com>
2019-11-08 03:45:21 +1030

    Improve logging for a failed patreon link

--- aiarena/patreon/views.py
+++ aiarena/patreon/views.py
@@ -27,49 +27,54 @@ class PatreonBindView(View):
 
 class PatreonCallbackView(View):
     def get(self, request):
-        oauth_client = PatreonOAuth(config.PATREON_CLIENT_ID, config.PATREON_CLIENT_SECRET)
-        site = Site.objects.get_current()
-        tokens = oauth_client.get_tokens(request.GET['code'], 'https://' + site.domain + '/patreon/oauth/redirect')
+        try:
+            oauth_client = PatreonOAuth(config.PATREON_CLIENT_ID, config.PATREON_CLIENT_SECRET)
+            site = Site.objects.get_current()
+            tokens = oauth_client.get_tokens(request.GET['code'], 'https://' + site.domain + '/patreon/oauth/redirect')
 
-        if 'access_token' in tokens and 'refresh_token' in tokens:
-            account_bind, created = PatreonAccountBind.objects.get_or_create(user=request.user)
-            account_bind.access_token = tokens['access_token']
-            account_bind.refresh_token = tokens['refresh_token']
-            account_bind.save()
-            messages.add_message(request, messages.SUCCESS, 'Patreon successfully linked.')
+            if 'access_token' in tokens and 'refresh_token' in tokens:
+                account_bind, created = PatreonAccountBind.objects.get_or_create(user=request.user)
+                account_bind.access_token = tokens['access_token']
+                account_bind.refresh_token = tokens['refresh_token']
+                account_bind.save()
+                messages.add_message(request, messages.SUCCESS, 'Patreon successfully linked.')
 
-            try:
-                api_client = PatreonApi(account_bind.access_token)
-                user = api_client.current_user()
-                patreon_level = 'none'
-                for entry in user['included']:
-                    if entry['type'] == 'pledge':
-                        if entry['attributes']['amount_cents'] >= 10000:  # diamond
-                            patreon_level = 'diamond'
-                            break
-                        elif entry['attributes']['amount_cents'] >= 5000:  # platinum
-                            patreon_level = 'platinum'
-                            break
-                        elif entry['attributes']['amount_cents'] >= 2500:  # gold
-                            patreon_level = 'gold'
-                            break
-                        elif entry['attributes']['amount_cents'] >= 1000:  # silver
-                            patreon_level = 'silver'
-                            break
-                        elif entry['attributes']['amount_cents'] >= 500:  # bronze
-                            patreon_level = 'bronze'
-                            break
-                        else:
-                            raise Exception("Unexpected Patreon tier encountered!")
+                try:
+                    api_client = PatreonApi(account_bind.access_token)
+                    user = api_client.current_user()
+                    patreon_level = 'none'
+                    for entry in user['included']:
+                        if entry['type'] == 'pledge':
+                            if entry['attributes']['amount_cents'] >= 10000:  # diamond
+                                patreon_level = 'diamond'
+                                break
+                            elif entry['attributes']['amount_cents'] >= 5000:  # platinum
+                                patreon_level = 'platinum'
+                                break
+                            elif entry['attributes']['amount_cents'] >= 2500:  # gold
+                                patreon_level = 'gold'
+                                break
+                            elif entry['attributes']['amount_cents'] >= 1000:  # silver
+                                patreon_level = 'silver'
+                                break
+                            elif entry['attributes']['amount_cents'] >= 500:  # bronze
+                                patreon_level = 'bronze'
+                                break
+                            else:
+                                raise Exception("Unexpected Patreon tier encountered!")
 
-                request.user.patreon_level = patreon_level
-                request.user.save()
-            except Exception as e:
-                logger.error("Failed to update patreon tier with error:\n" + traceback.print_exc())
-                messages.add_message(request, messages.WARNING, 'There was an issue updating your account\'s Patreon tier.')
+                    request.user.patreon_level = patreon_level
+                    request.user.save()
+                except Exception as e:
+                    logger.error("Failed to update patreon tier with error:\n" + traceback.print_exc())
+                    messages.add_message(request, messages.WARNING, 'There was an issue updating your account\'s Patreon tier.')
 
-        else:
-            logger.error("Patreon linkage failed. Tokens dump:\n" + json.dumps(tokens))
+            else:
+                logger.error("Patreon linkage failed. Tokens dump:\n" + json.dumps(tokens))
+                messages.add_message(request, messages.ERROR,
+                                     'Sorry, there was an issue linking your patreon. Please try again later. If this issue persists, please contact an admin via discord.')
+        except Exception as e:
+            logger.error("Patreon linkage failed with error:\n" + traceback.print_exc())
             messages.add_message(request, messages.ERROR,
                                  'Sorry, there was an issue linking your patreon. Please try again later. If this issue persists, please contact an admin via discord.')
 
