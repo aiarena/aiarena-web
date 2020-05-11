ac8f2c96e56db6b445885374eb4b0f52a3aa2cec
lladdy <randallboland@bigpond.com>
2020-05-12 05:04:45 +0930

    Add flag for requiring a trusted AC for a match.

--- aiarena/core/models/match.py
+++ aiarena/core/models/match.py
@@ -27,6 +27,8 @@ class Match(models.Model, LockableModelMixin):
     round = models.ForeignKey(Round, on_delete=models.CASCADE, blank=True, null=True)
     requested_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,
                                      related_name='requested_matches')
+    require_trusted_arenaclient = models.BooleanField(default=True)
+    """Whether this match should require it be run on a trusted arena client"""
 
     def __str__(self):
         return self.id.__str__()
