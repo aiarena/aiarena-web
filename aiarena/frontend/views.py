a08ebdf4b0ea5c7d4f74f8642ee67354600e6c55
lladdy <randallboland@bigpond.com>
2020-06-22 20:53:34 +0930

    Consolidate multiple bot update forms into one form.

--- aiarena/frontend/views.py
+++ aiarena/frontend/views.py
@@ -206,38 +206,27 @@ class BotSeasonStatsDetail(DetailView):
         return context
 
 
-class StandardBotUpdateForm(forms.ModelForm):
+class BotUpdateForm(forms.ModelForm):
+    """
+    Standard form for updating a bot
+    """
     wiki_article_content = forms.CharField(label='Bot page content', required=False, widget=getEditor().get_widget())
 
-    class Meta:
-        model = Bot
-        fields = ['active', 'bot_zip', 'bot_zip_publicly_downloadable', 'bot_data',
-                  'bot_data_publicly_downloadable']
+    def __init__(self, *args, **kwargs, ):
+        super().__init__(*args, **kwargs)
 
+        # change the available fields based upon whether the bot_data is available for editing or not
+        # and whether there's a current season
+        if self.instance.bot_data_is_currently_frozen():
+            self.fields['bot_data'].disabled = True
 
-class NoCurrentSeasonBotUpdateForm(forms.ModelForm):
-    active = forms.BooleanField(disabled=True, required=False)
-    wiki_article_content = forms.CharField(label='Bot page content', required=False, widget=getEditor().get_widget())
-
-    class Meta:
-        model = Bot
-        fields = ['active', 'bot_zip', 'bot_zip_publicly_downloadable', 'bot_data', 'bot_data_publicly_downloadable']
-
-
-class FrozenDataBotUpdateForm(forms.ModelForm):
-    bot_data = forms.FileField(disabled=True)
-    wiki_article_content = forms.CharField(label='Bot page content', required=False, widget=getEditor().get_widget())
-
-    class Meta:
-        model = Bot
-        fields = ['active', 'bot_zip', 'bot_zip_publicly_downloadable', 'bot_data',
-                  'bot_data_publicly_downloadable']
-
-
-class NoCurrentSeasonFrozenDataBotUpdateForm(forms.ModelForm):
-    active = forms.BooleanField(disabled=True)
-    bot_data = forms.FileField(disabled=True)
-    wiki_article_content = forms.CharField(label='Bot page content', required=False, widget=getEditor().get_widget())
+        try:
+            Season.get_current_season()
+            # in season
+        except NoCurrentSeason:
+            # outside season - don't allow activation
+            self.fields['active'].disabled = True
+            self.fields['active'].required = False
 
     class Meta:
         model = Bot
@@ -247,6 +236,7 @@ class NoCurrentSeasonFrozenDataBotUpdateForm(forms.ModelForm):
 
 class BotUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     template_name = 'bot_edit.html'
+    form_class = BotUpdateForm
 
     redirect_field_name = 'next'
     success_message = "Bot saved successfully"
@@ -259,22 +249,6 @@ class BotUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse('bot_edit', kwargs={'pk': self.object.pk})
-
-    # change the available fields based upon whether the bot_data is available for editing or not
-    # and whether there's a current season
-    def get_form_class(self):
-        if self.object.bot_data_is_currently_frozen():
-            try:
-                Season.get_current_season()
-                return FrozenDataBotUpdateForm
-            except NoCurrentSeason:
-                return NoCurrentSeasonFrozenDataBotUpdateForm
-        else:
-            try:
-                Season.get_current_season()
-                return StandardBotUpdateForm
-            except NoCurrentSeason:
-                return NoCurrentSeasonBotUpdateForm
 
     def get_form(self, form_class=None):
         """Return an instance of the form to be used in this view."""
