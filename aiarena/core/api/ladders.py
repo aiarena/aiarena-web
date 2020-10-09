d00835841025702374734a9798856a2c46da7fa2
eladyaniv01 <eladyaniv01@gmail.com>
2020-10-09 16:02:26 +0300

    refactor: ladder.py now does not use SeasonParticipant, as its removed,  TODO -  new logic for grabbing top 10 bots
    index.html template updated to accomodate the context change

--- aiarena/core/api/ladders.py
+++ aiarena/core/api/ladders.py
@@ -2,7 +2,7 @@ import logging
 
 from django.db.models import Max
 
-from aiarena.core.models import Season, SeasonParticipation, Round
+from aiarena.core.models import Bot, Season, Round
 
 logger = logging.getLogger(__name__)
 
@@ -10,18 +10,22 @@ logger = logging.getLogger(__name__)
 class Ladders:
     @staticmethod
     def get_season_ranked_participants(season: Season, amount=None):
-        # only return SeasonParticipations that are included in the most recent round
-        last_round = Ladders.get_most_recent_round(season)
-        limit = f" LIMIT {amount}" if amount else ""
-        query = "select distinct csp.id, elo, bot_id, win_perc, slug " \
-                "from core_seasonparticipation csp " \
-                "where season_id = %s and bot_id in (" \
-                "select cmp.bot_id " \
-                "from core_match cm " \
-                "join core_matchparticipation cmp on cm.id = cmp.match_id " \
-                "where cm.round_id = %s)" \
-                " order by csp.elo desc " + limit
-        return SeasonParticipation.objects.raw(query, (season.id, last_round.id,))
+        # TODO need to filter by current season and return a season stats object
+
+        bots = Bot.objects.all()
+        return bots[:10]
+
+        # last_round = Ladders.get_most_recent_round(season)
+        # limit = f" LIMIT {amount}" if amount else ""
+        # query = "select distinct csp.id, elo, bot_id, win_perc, slug " \
+        #         "from core_seasonparticipation csp " \
+        #         "where season_id = %s and bot_id in (" \
+        #         "select cmp.bot_id " \
+        #         "from core_match cm " \
+        #         "join core_matchparticipation cmp on cm.id = cmp.match_id " \
+        #         "where cm.round_id = %s)" \
+        #         " order by csp.elo desc " + limit
+        # return SeasonParticipation.objects.raw(query, (season.id, last_round.id,))
 
     @staticmethod
     def get_most_recent_round(season: Season):
