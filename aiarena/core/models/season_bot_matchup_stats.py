f231ae04ffd34b1714123d272d48bd49f27e3b47
eladyaniv01 <eladyaniv01@gmail.com>
2020-10-09 16:12:43 +0300

    refactor: SeasonBotMatchupStats  -  replace bot , opponent  with bot1, bot2

--- aiarena/core/models/season_bot_matchup_stats.py
+++ aiarena/core/models/season_bot_matchup_stats.py
@@ -2,15 +2,18 @@ import logging
 
 from django.db import models
 
+from aiarena.core.models import Bot
 from aiarena.core.validators import validate_not_nan, validate_not_inf
-from .season_participation import SeasonParticipation
+# from .season_participation import SeasonParticipation
 
 logger = logging.getLogger(__name__)
 
 
 class SeasonBotMatchupStats(models.Model):
-    bot = models.ForeignKey(SeasonParticipation, on_delete=models.CASCADE, related_name='season_matchup_stats')
-    opponent = models.ForeignKey(SeasonParticipation, on_delete=models.CASCADE, related_name='opponent_matchup_stats')
+    bot = None  # Dan ,  the DB expects bot_id to be in this table,  any suggestions ?
+    opponent = None
+    bot1 = models.ForeignKey(Bot, on_delete=models.CASCADE, related_name='bot1')
+    bot2 = models.ForeignKey(Bot, on_delete=models.CASCADE, related_name='bot2')
     match_count = models.IntegerField(blank=True, null=True)
     win_count = models.IntegerField(blank=True, null=True)
     win_perc = models.FloatField(blank=True, null=True, validators=[validate_not_nan, validate_not_inf])
