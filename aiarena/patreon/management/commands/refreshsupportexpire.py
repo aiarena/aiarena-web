7f55bccd493c3b970fbb1377ce29d8a85375947a
Elad <eladyaniv01@gmail.com>
2020-10-12 21:13:07 +0300

    feat: added custom command to refresh supporter status when it expires

--- aiarena/patreon/management/commands/refreshsupportexpire.py
+++ aiarena/patreon/management/commands/refreshsupportexpire.py
@@ -1,6 +1,7 @@
 import os
 import traceback
-
+from datetime import timedelta, datetime
+from pytz import UTC
 from django.core.management.base import BaseCommand, CommandError
 
 from aiarena.core.models import User
@@ -13,13 +14,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-
+        now = datetime.astimezone(datetime.now(), tz=UTC)
         for user in User.objects.all():
             if user.supported_expiration_date is not None:
-                # TODO some code to assert the date is in the future
-                # if the date is in the past , present
-                # turn user.supported_expiration_date back to None
-                # TODO Test this
-                user.save()
+                delta = user.supported_expiration_date - now
+                days = delta.days
+                if days <=0:
+                    user.supported_expiration_date = None
+                    user.save()
+
+
 
 
