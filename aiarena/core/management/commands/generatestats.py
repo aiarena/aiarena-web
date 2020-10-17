3e712034bc5edc8d650f027e306f89ba7879622a
Elad <eladyaniv01@gmail.com>
2020-10-17 16:29:03 +0300

    feat: add option to generate stats for all seasons

--- aiarena/core/management/commands/generatestats.py
+++ aiarena/core/management/commands/generatestats.py
@@ -16,21 +16,29 @@ class Command(BaseCommand):
         parser.add_argument('--seasonid', type=int, help="The season id t generate stats for. "
                                                        "If this isn't supplied the currently "
                                                        "active season will ne used")
+        parser.add_argument('--allseasons', type=bool, help="Run this for all seasons")
 
     def handle(self, *args, **options):
         bot_id = options['botid']
-        season_id = options['seasonid'] if options['seasonid'] is not None else Season.get_current_season().id
-        if bot_id is not None:
-            sp = SeasonParticipation.objects.get(season_id=season_id, bot_id=bot_id)
-            with transaction.atomic():
-                sp.lock_me()
-                self.stdout.write(f'Generating current season stats for bot {bot_id}...')
-                StatsGenerator.update_stats(sp)
+
+        if options['allseasons']:
+            seasons = Season.objects.all()
         else:
-            for sp in SeasonParticipation.objects.filter(season_id=season_id):
-                with transaction.atomic():
-                    sp.lock_me()
-                    self.stdout.write(f'Generating current season stats for bot {sp.bot_id}...')
-                    StatsGenerator.update_stats(sp)
+            seasons = [options['seasonid'] if options['seasonid'] is not None else Season.get_current_season().id]
+
+            for s in seasons:
+                season_id = s.id
+                if bot_id is not None:
+                    sp = SeasonParticipation.objects.get(season_id=season_id, bot_id=bot_id)
+                    with transaction.atomic():
+                        sp.lock_me()
+                        self.stdout.write(f'Generating current season stats for bot {bot_id}...')
+                        StatsGenerator.update_stats(sp)
+                else:
+                    for sp in SeasonParticipation.objects.filter(season_id=season_id):
+                        with transaction.atomic():
+                            sp.lock_me()
+                            self.stdout.write(f'Generating current season stats for bot {sp.bot_id}...')
+                            StatsGenerator.update_stats(sp)
 
         self.stdout.write('Done')
