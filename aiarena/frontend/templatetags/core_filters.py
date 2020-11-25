de3b29e76ba37664d5cafab43c5b1b3e7057c3a6
Immodal <kychin.n@gmail.com>
2020-11-25 16:04:40 +1100

    Added highlights to bot avg step time
     - Added step_time_color to core_filters.py
     - Added color style to relevant cell in bot.html

--- aiarena/frontend/templatetags/core_filters.py
+++ aiarena/frontend/templatetags/core_filters.py
@@ -39,7 +39,33 @@ def smooth_timedelta(timedeltaobj):
     return timetot
 
 
+def step_time_color(value):
+    """Generate color for given step time(ms)."""
+    def get_color(w1, c1, c2): # Gradient between c1 and c2
+        w1 = 0 if w1<=0 else 1 if w1>=1 else w1
+        w2 = 1 - w1
+        rgb = [hex(round(c1[i] * w1 + c2[i] * w2))[2:] for i in range(3)]
+        return [("0"+h) if len(h)<2 else h for h in rgb] # make sure always 2 chars long
+
+    high_color = (255, 0, 0) # Red
+    mid_color = (255, 255, 0) # Yellow
+    low_color = (0, 255, 0) # Green
+
+    rgb = ["ff", "ff", "ff"] # Default White
+    minpoint = 10 # Step times
+    midpoint = 30
+    maxpoint = 50
+    if value>midpoint:
+        weight = (value-midpoint)/(maxpoint-midpoint)
+        rgb = get_color(weight, high_color, mid_color)
+    elif value<=midpoint:
+        weight = (value-minpoint)/(midpoint-minpoint)
+        rgb = get_color(weight, mid_color, low_color)
+    return f"#{''.join(rgb)}"
+
+
 register = template.Library()
 register.filter('format_elo_change', format_elo_change)
 register.filter('smooth_timedelta', smooth_timedelta)
 register.filter('cents_to_usd', cents_to_usd)
+register.filter('step_time_color', step_time_color)
