e8068df0adac14c86be748a7482643d9bea91a66
eladyaniv01 <40754127+eladyaniv01@users.noreply.github.com>
2020-11-27 16:08:46 +0200

    revert migration hack
     to not double create the same table

--- aiarena/core/migrations/0121_auto_20201124_0720.py
+++ aiarena/core/migrations/0121_auto_20201124_0720.py
@@ -10,18 +10,18 @@ class Migration(migrations.Migration):
         ('core', '0120_auto_20201124_0335'),
     ]
 
-#     operations = [
-#         migrations.CreateModel(
-#             name='TrophyIcon',
-#             fields=[
-#                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-#                 ('name', models.CharField(max_length=64, unique=True)),
-#                 ('image', models.ImageField(upload_to='trophy_images/')),
-#             ],
-#         ),
-#         migrations.AddField(
-#             model_name='trophy',
-#             name='icon',
-#             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='core.TrophyIcon'),
-#         ),
-#     ]
+    operations = [
+        migrations.CreateModel(
+            name='TrophyIcon',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=64, unique=True)),
+                ('image', models.ImageField(upload_to='trophy_images/')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='trophy',
+            name='icon',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='core.TrophyIcon'),
+        ),
+    ]
