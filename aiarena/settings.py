3237d47ae5b0b992a552aaeb654773315bcb8b87
eladyaniv01 <40754127+eladyaniv01@users.noreply.github.com>
2020-12-28 14:23:56 +0200

    Update settings.py

--- aiarena/settings.py
+++ aiarena/settings.py
@@ -158,6 +158,13 @@ REST_FRAMEWORK = {
 }
 
 CACHES = {
+    "default": {	
+        "BACKEND": "django_redis.cache.RedisCache",	
+        "LOCATION": "redis://127.0.0.1:6379/",	
+        "OPTIONS": {	
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",	
+        }	
+    },
     'select2': {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/2",
