c435a55ca015a1848f9246c9734a7c3c0a199e4b
eladyaniv01 <40754127+eladyaniv01@users.noreply.github.com>
2020-12-28 14:16:07 +0200

    attempt to make redis play well with ACs

--- aiarena/settings.py
+++ aiarena/settings.py
@@ -158,13 +158,6 @@ REST_FRAMEWORK = {
 }
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    },
     'select2': {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/2",
