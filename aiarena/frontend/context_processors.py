b7fea66e140f39fdb91b5166cf767f5001d1cda9
Elad <eladyaniv01@gmail.com>
2021-01-31 13:36:41 +0200

    fix: wrong path in remote enviroments for style.css

--- aiarena/frontend/context_processors.py
+++ aiarena/frontend/context_processors.py
@@ -30,6 +30,7 @@ def style_md5() -> str:
         except Exception as e:
             # in remote envs we provide the absolute path
             path = '/home/aiarena/ai-arena/aiarena/frontend/static/style.css'
+            md5_value = md5(path)[0:8]
         cache.set('style_md5', 'md5_value', 3600)
     return md5_value
 
