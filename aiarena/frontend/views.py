50ce56e92dc3fd5542b29b0a755ea03ab5d3a3c8
Immodal <kychin.n@gmail.com>
2021-04-16 22:43:31 +1000

    Limit number of tags per match per user

--- aiarena/frontend/views.py
+++ aiarena/frontend/views.py
@@ -737,7 +737,7 @@ class MatchTagForm(forms.Form):
     def clean_tags(self):
         """convert tags from single string to list"""
         data = self.cleaned_data['tags'].lower().split(",")
-        return [re.sub('[^a-zA-Z0-9 _]', '', tag.strip())[:32] for tag in data if tag]
+        return [re.sub('[^a-zA-Z0-9 _]', '', tag.strip())[:32] for tag in data if tag][:32]
 
 
 class MatchTagFormView(SingleObjectMixin, FormView):
