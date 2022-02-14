faabc368c5c07fb9be95d4970b0c5951a7ebe13e
lladdy <randallboland@bigpond.com>
2022-02-14 21:02:19 +1030

    Add page for displaying downloadable bots.

--- aiarena/frontend/views.py
+++ aiarena/frontend/views.py
@@ -229,6 +229,23 @@ class BotList(ListView):
         return context
 
 
+class BotDownloadableList(ListView):
+    queryset = Bot.objects.filter(bot_zip_publicly_downloadable=True)\
+        .only('name', 'plays_race', 'type', 'user__username', 'user__type')\
+        .select_related('user').order_by('name')
+    template_name = 'bots_downloadable.html'
+    paginate_by = 50
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # add the page ranges
+        page_obj = context['page_obj']
+        context['page_range'] = restrict_page_range(page_obj.paginator.num_pages, page_obj.number)
+
+        return context
+
+
 class FileURLColumn(tables.URLColumn):
     """File URLs are incorrect without this"""
     def get_url(self, value):
