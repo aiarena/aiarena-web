d7cea74ae54590d537ce04a9e2b8ed3eefad8ac4
lladdy <randallboland@bigpond.com>
2022-02-23 08:29:51 +1030

    Re-term donator to supporter.

--- aiarena/core/models/user.py
+++ aiarena/core/models/user.py
@@ -105,10 +105,20 @@ class User(AbstractUser, LockableModelMixin):
 
     @property
     def has_donated(self):
+        """
+        Whether the user has donated funds to the project.
+        """
         return self.patreon_level != 'none'
 
+    @property
+    def is_supporter(self):
+        """
+        In future this will include logic for determining general support - not just financial.
+        """
+        return self.has_donated
+
     @staticmethod
-    def random_donator():
+    def random_supporter():
         # todo: apparently order_by('?') is really slow
         # https://stackoverflow.com/questions/962619/how-to-pull-a-random-record-using-djangos-orm#answer-962672
         return User.objects.only('id', 'username').exclude(patreon_level='none').order_by('?').first()
