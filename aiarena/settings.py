b3b27a605ea33476ee1406cb741380346789f000
lladdy <randallboland@bigpond.com>
2022-03-21 03:43:20 +1030

    Revert change to settings HOST

--- aiarena/settings.py
+++ aiarena/settings.py
@@ -51,7 +51,7 @@ DATABASES = {
         'NAME': os.getenv('MYSQL_DATABASE', 'aiarena'),
         'USER': os.getenv('MYSQL_USER', 'aiarena'),
         'PASSWORD': os.getenv('MYSQL_PASSWORD', 'aiarena'),
-        'HOST': os.getenv('MYSQL_HOST', 'localhost'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
         'PORT': os.getenv('MYSQL_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
