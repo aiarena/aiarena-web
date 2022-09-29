c94807792cb36d98a02183d96ec1815281874957
lladdy <randallboland@bigpond.com>
2022-09-29 16:02:01 +0930

    Attempt to resolve github migration merge complaint

--- aiarena/core/migrations/0055_trusted_competition.py
+++ aiarena/core/migrations/0055_trusted_competition.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0051_competition_playable_races'),
+        ('core', '0054_rename_plays_race_model_bot_plays_race'),
     ]
 
     operations = [
