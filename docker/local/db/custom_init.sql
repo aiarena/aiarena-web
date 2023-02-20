6a3e9555c9d6b7c9eb4932bf42e744f29ae454ee
lladdy <randallboland@bigpond.com>
2023-02-20 21:03:45 +1030

    feat: simplify local docker compose setup

--- docker/local/db/custom_init.sql
+++ docker/local/db/custom_init.sql
@@ -1,3 +1,3 @@
 create database aiarena;
-create user aiarena with encrypted password 'aiarena';
+create user aiarena with encrypted password 'aiarena' createdb;
 grant all privileges on database aiarena to aiarena;
--- docker/local/db/custom_test_init.sql
+++
@@ -1,3 +0,0 @@
-create database test_aiarena;
-create user aiarena with encrypted password 'aiarena';
-grant all privileges on database test_aiarena to aiarena;
