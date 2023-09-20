a552855187c27f58457cca0c0aa39d8760c60610
Ilia Peterov <ipeterov1@gmail.com>
2023-09-20 14:45:36 +0400

    Temporarily enable debug-toolbar on aiairena-test.net

--- aiarena/settings/prod.py
+++ aiarena/settings/prod.py
@@ -5,6 +5,18 @@ from .default import *  # noqa: F403
 
 DEBUG = False
 
+INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+
+
+def custom_show_toolbar(request):
+    return True  # Always show toolbar, for example purposes only.
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": custom_show_toolbar,
+}
+
 SECRET_KEY = os.environ.get("SECRET_KEY", None)
 if SECRET_KEY is None:
     raise Exception("You must set the SECRET_KEY to something secure before running in production or staging.")
