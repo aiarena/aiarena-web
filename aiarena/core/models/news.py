3094a26ebcabf5c98c0f2ced55690874530ffe25
lladdy <randallboland@bigpond.com>
2023-10-17 20:29:40 +1030

    chore: improve selection performance on news and match participations

--- aiarena/core/models/news.py
+++ aiarena/core/models/news.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class News(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
     title = models.TextField(max_length=40, blank=True, null=True)
     text = models.TextField(max_length=500, blank=False, null=False)
     yt_link = models.URLField(blank=True, null=True)
