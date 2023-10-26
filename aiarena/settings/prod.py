1e83dabf1586165d8ef6799fad7df100ed678178
Ilia Peterov <ipeterov1@gmail.com>
2023-10-26 15:28:25 +0400

    Enable sentry tracing

--- aiarena/settings/prod.py
+++ aiarena/settings/prod.py
@@ -55,8 +55,7 @@ if SENTRY_DSN:
         "release": BUILD_NUMBER,
         "send_default_pii": True,
         # https://docs.sentry.io/performance/distributed-tracing/#python
-        # https://ivelum.slack.com/archives/C0PT3267R/p1675054580786709
-        "traces_sample_rate": 0.0,
+        "traces_sample_rate": 1.0,
         "attach_stacktrace": True,
     }
     sentry_sdk.init(**sentry_kwargs)
