1549cd49f087cc0899f5f2d42f5cffbc7f33bb58
Ilia Peterov <ipeterov1@gmail.com>
2023-11-23 14:18:35 +0400

    chore: Trigger re-deployment

--- aiarena/core/middleware.py
+++ aiarena/core/middleware.py
@@ -4,10 +4,6 @@ from django.shortcuts import render
 
 def maintenance(get_response):
     def middleware(request):
-        response = HttpResponse(
-            render(request, "maintenance.html"),
-            status=200,
-        )
-        return response
+        return HttpResponse(render(request, "maintenance.html"), status=200)
 
     return middleware
