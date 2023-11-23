4802fc4f47ea52cb63db4c19bbe0d6e0607105b0
Ilia Peterov <ipeterov1@gmail.com>
2023-11-23 17:16:19 +0400

    fix: Don't use the deprecated ugettext_lazy in django admin

--- aiarena/frontend/dashboard.py
+++ aiarena/frontend/dashboard.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from grappelli.dashboard import Dashboard, modules
 from grappelli.dashboard.utils import get_admin_site_name
