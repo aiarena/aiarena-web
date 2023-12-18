2d78015e387a772db7e35a45886af19030a221f0
lladdy <randallboland@bigpond.com>
2023-12-19 00:43:53 +1030

    chore: enhance the bot upload example

--- aiarena/api/examples/upload_new_bot_version.py
+++ aiarena/api/examples/upload_new_bot_version.py
@@ -1,21 +1,31 @@
 import requests
 
 
-bot_id = 1  # insert bot id
-token = "<token here>"  # insert token you received from admin
-auth = {"Authorization": f"Token {token}"}
+my_bot_id = 1  # You can obtain this from the URL of your bot's page: https://aiarena.net/bots/<bot_id>/
+token = "<token here>"  # insert the token you received from your token page: https://aiarena.net/profile/token/
+my_bot_zip_file = "./bot_zip.zip"  # the file path to your bot zip
+my_bot_data_zip_file = "./bot_data.zip"  # the file path to your bot's new data zip
+my_bot_article_content = """  
+    # My bot
+    This is my bot. It is very good.
+"""  # this content appears on your bot's page, under the "Biography" header
+my_bot_zip_publicly_downloadable = True  # setting this to False will stop other users downloading your bot zip
+my_bot_data_publicly_downloadable = True  # setting this to False will stop other users downloading your bot data
 
-if __name__ == "__main__":
-    with open("./my_bot.zip", "rb") as f:
-        response = requests.patch(
-            f"https://aiarena.net/api/bots/{bot_id}/",
-            headers=auth,
-            data={
-                "bot_zip_publicly_downloadable": False,
-                "bot_data_publicly_downloadable": False,
-            },
-            files={
-                "bot_zip": f,
-            },
-        )
-        print(response.json())
+
+auth = {"Authorization": f"Token {token}"}
+with open(my_bot_zip_file, "rb") as bot_zip, open(my_bot_data_zip_file, "rb") as bot_data:
+    response = requests.patch(
+        f"https://aiarena.net/api/bots/{my_bot_id}/",
+        headers=auth,
+        data={
+            "bot_zip_publicly_downloadable": my_bot_zip_publicly_downloadable,
+            "bot_data_publicly_downloadable": my_bot_data_publicly_downloadable,
+            "wiki_article_content": my_bot_article_content,
+        },
+        files={
+            "bot_zip": bot_zip,
+            "bot_data": bot_data,
+        },
+    )
+    print(response.json())
