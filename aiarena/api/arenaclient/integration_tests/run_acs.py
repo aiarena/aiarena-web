90e9aa592a67eb4f624586ac8d3206e86a3abef9
lladdy <randallboland@bigpond.com>
2024-02-25 21:28:12 +1030

    chore: add args to integration tests script

--- aiarena/api/arenaclient/integration_tests/run_acs.py
+++ aiarena/api/arenaclient/integration_tests/run_acs.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import multiprocessing
 import sys
@@ -16,42 +17,52 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-NUM_ACS = 10
-NUM_MATCHES_TO_RUN = 100
-API_URL = "http://127.0.0.1:8000"
-AC_SLEEP_TIME = 0.1
+
+def retrieve_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--num_acs", type=int, default=10)
+    arg_parser.add_argument("--num_matches_to_run", type=int, default=1000)
+    arg_parser.add_argument("--api_url", type=str, default="http://127.0.0.1:8000")
+    arg_parser.add_argument("--ac_sleep_time", type=float, default=0.0)
+    return arg_parser.parse_args()
 
 
-def run_ac(ac_id, num_matches, lock):
+def run_ac(ac_id, num_matches, lock, total_matches_to_run, api_url, ac_sleep_time):
     ac_id = str(ac_id)
     ac_token = ac_id
 
     logger.info(f"AC {ac_id} - Starting")
-    ac = MockArenaClient(ac_id=ac_id, webserver_url=API_URL, api_token=ac_token, working_dir=f"./tmp/{ac_id}/")
+    ac = MockArenaClient(ac_id=ac_id, webserver_url=api_url, api_token=ac_token, working_dir=f"./tmp/{ac_id}/")
 
     while True:
         with lock:  # reserve a match for this AC if there are any left to play
-            if num_matches.value >= NUM_MATCHES_TO_RUN:
+            if num_matches.value >= total_matches_to_run:
                 logger.info(f"AC {ac_id} - Exiting: no more matches to run.")
                 break
 
             num_matches.value += 1
             logger.info(f"AC {ac_id} - Starting match {num_matches.value}")
-            logger.info(f"AC {ac_id} - {NUM_MATCHES_TO_RUN-num_matches.value} matches left to run")
+            logger.info(f"AC {ac_id} - {total_matches_to_run - num_matches.value} matches left to run")
 
         # don't quit until this match is run
         while not ac.run_a_match():
             logger.debug(f"AC {ac_id} is sleeping")
-            sleep(AC_SLEEP_TIME)
+            sleep(ac_sleep_time)
 
 
 if __name__ == "__main__":
+    args = retrieve_args()
     processes = []
-    num_matches = multiprocessing.Value("i", 0)
+    matches_played_count = multiprocessing.Value("i", 0)
     lock = multiprocessing.Lock()
 
-    for ac_id in range(NUM_ACS):
-        processes.append(Process(target=run_ac, args=(ac_id, num_matches, lock)))
+    for ac_id in range(args.num_acs):
+        processes.append(
+            Process(
+                target=run_ac,
+                args=(ac_id, matches_played_count, lock, args.num_matches_to_run, args.api_url, args.ac_sleep_time),
+            )
+        )
 
     for p in processes:
         p.start()
@@ -59,4 +70,4 @@ if __name__ == "__main__":
     for p in processes:
         p.join()
 
-    logger.info(f"Total matches played: {num_matches.value}")
+    logger.info(f"Total matches played: {matches_played_count.value}")
