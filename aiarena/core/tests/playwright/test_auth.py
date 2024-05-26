262dc3b2fd61c25763261dc4edc52e4ef960219e
Ilia Peterov <ipeterov1@gmail.com>
2024-05-26 20:50:23 +0400

    chore: first Playwright tests - test log in form

---
+++ aiarena/core/tests/playwright/test_auth.py
@@ -0,0 +1,25 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+from ..base import BrowserHelper
+
+
+pytestmark = [pytest.mark.playwright]
+
+
+def test_login(page: Page, bh: BrowserHelper, user, admin_user):
+    page.goto(bh.reverse("login"))
+    page.get_by_label("Username:").fill("billy")
+    page.get_by_label("Password:").fill("guest")
+    page.get_by_role("button", name="Log in").click()
+    expect(page.locator("#sidebar-items")).to_contain_text("Logged in: billy")
+
+
+def test_login_with_wrong_credentials(page: Page, bh: BrowserHelper):
+    page.goto(bh.reverse("login"))
+    page.get_by_label("Username:").fill("test")
+    page.get_by_label("Password:").fill("123456")
+    page.get_by_role("button", name="Log in").click()
+    expect(page.locator("form")).to_contain_text(
+        "Please enter a correct username and password. Note that both fields may be case-sensitive."
+    )
