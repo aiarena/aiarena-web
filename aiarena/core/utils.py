50be5ee5ed4da17d27dfe86efda2a8ed956e56be
Ilia Peterov <ipeterov1@gmail.com>
2024-07-07 14:28:52 +0400

    chore: remove post_result_to_discord_bot

--- aiarena/core/utils.py
+++ aiarena/core/utils.py
@@ -1,10 +1,6 @@
-import json
 import logging
-import os
 import re
 import time
-import traceback
-from urllib import request
 
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
@@ -96,55 +92,6 @@ def calculate_md5_django_filefield(file, block_size=2**20):
             md5.update(data)
 
     return md5.hexdigest()
-
-
-def post_result_to_discord_bot(result):
-    try:
-        participants = result.get_match_participants
-        bots = result.get_match_participant_bots
-
-        if result.has_winner:
-            wl_bots = result.get_winner_loser_bots
-        else:
-            wl_bots = None
-
-        json = {  # todo: nested json
-            "match_id": result.match.id,
-            "round_id": result.match.round_id,
-            "bot1": bots[0].name,
-            "bot1_id": bots[0].id,
-            "bot1_resultant_elo": participants[0].resultant_elo,
-            "bot1_elo_change": participants[0].elo_change,
-            "bot1_avg_step_time": participants[0].avg_step_time,
-            "bot2": bots[1].name,
-            "bot2_id": bots[1].id,
-            "bot2_resultant_elo": participants[1].resultant_elo,
-            "bot2_elo_change": participants[1].elo_change,
-            "bot2_avg_step_time": participants[1].avg_step_time,
-            "winner": wl_bots[0].name if wl_bots is not None else None,
-            "loser": wl_bots[1].name if wl_bots is not None else None,
-            "result_type": result.type,
-            "game_steps": result.game_steps,
-            "replay_file_download_url": result.replay_file.url if result.replay_file else None,
-        }
-        post_json_content_to_address(json, settings.POST_SUBMITTED_RESULTS_TO_ADDRESS)
-    except Exception:
-        logger.warning(
-            f"Attempt to post result for match_id {result.match.id} to discord failed with error:"
-            + os.linesep
-            + traceback.format_exc()
-        )
-
-
-def post_json_content_to_address(json_content, address):
-    req = request.Request(address)
-    req.add_header("Content-Type", "application/json; charset=utf-8")
-    jsondata = json.dumps(json_content)
-    jsondataasbytes = jsondata.encode("utf-8")  # needs to be bytes
-    req.add_header("Content-Length", len(jsondataasbytes))
-    print(jsondataasbytes)
-    request.urlopen(req, jsondataasbytes)
-    # todo: check response
 
 
 # ELO Implementation:
