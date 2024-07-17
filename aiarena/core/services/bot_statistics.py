0bdbe42472a40c5db670c9546d0bd16234c0b366
Ilia Peterov <ipeterov1@gmail.com>
2024-07-18 02:17:09 +0400

    chore: refactor maps matchup recalculation - consistent naming of self and opponent participations

--- aiarena/core/services/bot_statistics.py
+++ aiarena/core/services/bot_statistics.py
@@ -22,125 +22,127 @@ class BotStatistics:
     _ignored_result_types = ["MatchCancelled", "InitializationError", "Error"]
 
     def update_stats_based_on_result(
-        self, bot: CompetitionParticipation, result: Result, opponent: CompetitionParticipation
+        self, self_p: CompetitionParticipation, result: Result, opponent_p: CompetitionParticipation
     ):
         """This method updates a bot's existing stats based on a single result.
         This can be done much quicker that regenerating a bot's entire set of stats"""
 
-        if result.type not in self._ignored_result_types and bot.competition.indepth_bot_statistics_enabled:
-            with advisory_lock(f"stats_lock_competitionparticipation_{bot.id}") as acquired:
+        if result.type not in self._ignored_result_types and self_p.competition.indepth_bot_statistics_enabled:
+            with advisory_lock(f"stats_lock_competitionparticipation_{self_p.id}") as acquired:
                 if not acquired:
                     raise Exception(
-                        "Could not acquire lock on bot statistics for competition participation " + str(bot.id)
+                        "Could not acquire lock on bot statistics for competition participation " + str(self_p.id)
                     )
-                self._update_global_statistics(bot, result)
-                self._update_matchup_stats(bot, opponent, result)
-                self._update_map_stats(bot, result)
+                self._update_global_statistics(self_p, result)
+                self._update_matchup_stats(self_p, opponent_p, result)
+                self._update_map_stats(self_p, result)
 
-    def recalculate_stats(self, sp: CompetitionParticipation):
+    def recalculate_stats(self, self_p: CompetitionParticipation):
         """This method entirely recalculates a bot's set of stats."""
 
-        with advisory_lock(f"stats_lock_competitionparticipation_{sp.id}") as acquired:
+        with advisory_lock(f"stats_lock_competitionparticipation_{self_p.id}") as acquired:
             if not acquired:
-                raise Exception(f"Could not acquire lock on bot statistics for competition participation  {str(sp.id)}")
+                raise Exception(
+                    f"Could not acquire lock on bot statistics for competition participation  {str(self_p.id)}"
+                )
 
-            self._recalculate_global_statistics(sp)
+            self._recalculate_global_statistics(self_p)
 
-            if sp.competition.indepth_bot_statistics_enabled:
-                self._recalculate_matchup_stats(sp)
-                self._recalculate_map_stats(sp)
+            if self_p.competition.indepth_bot_statistics_enabled:
+                self._recalculate_matchup_stats(self_p)
+                self._recalculate_map_stats(self_p)
 
-    def _recalculate_global_statistics(self, sp: CompetitionParticipation):
-        sp.match_count = (
+    def _recalculate_global_statistics(self, self_p: CompetitionParticipation):
+        self_p.match_count = (
             MatchParticipation.objects.filter(
-                bot=sp.bot, match__result__isnull=False, match__round__competition=sp.competition
+                bot=self_p.bot, match__result__isnull=False, match__round__competition=self_p.competition
             )
             .exclude(match__result__type__in=self._ignored_result_types)
             .count()
         )
-        if sp.match_count != 0:
-            sp.win_count = MatchParticipation.objects.filter(
-                self.result_win, bot=sp.bot, match__round__competition=sp.competition
+        if self_p.match_count != 0:
+            self_p.win_count = MatchParticipation.objects.filter(
+                self.result_win, bot=self_p.bot, match__round__competition=self_p.competition
             ).count()
-            sp.win_perc = sp.win_count / sp.match_count * 100
+            self_p.win_perc = self_p.win_count / self_p.match_count * 100
 
-            if sp.competition.indepth_bot_statistics_enabled:
-                sp.loss_count = MatchParticipation.objects.filter(
-                    self.result_loss, bot=sp.bot, match__round__competition=sp.competition
+            if self_p.competition.indepth_bot_statistics_enabled:
+                self_p.loss_count = MatchParticipation.objects.filter(
+                    self.result_loss, bot=self_p.bot, match__round__competition=self_p.competition
                 ).count()
-                sp.loss_perc = sp.loss_count / sp.match_count * 100
-                sp.tie_count = MatchParticipation.objects.filter(
-                    self.result_tie, bot=sp.bot, match__round__competition=sp.competition
+                self_p.loss_perc = self_p.loss_count / self_p.match_count * 100
+                self_p.tie_count = MatchParticipation.objects.filter(
+                    self.result_tie, bot=self_p.bot, match__round__competition=self_p.competition
                 ).count()
-                sp.tie_perc = sp.tie_count / sp.match_count * 100
-                sp.crash_count = MatchParticipation.objects.filter(
+                self_p.tie_perc = self_p.tie_count / self_p.match_count * 100
+                self_p.crash_count = MatchParticipation.objects.filter(
                     self.result_crash,
-                    bot=sp.bot,
-                    match__round__competition=sp.competition,
+                    bot=self_p.bot,
+                    match__round__competition=self_p.competition,
                 ).count()
-                sp.crash_perc = sp.crash_count / sp.match_count * 100
+                self_p.crash_perc = self_p.crash_count / self_p.match_count * 100
 
-                sp.highest_elo = (
+                self_p.highest_elo = (
                     MatchParticipation.objects.filter(
-                        bot=sp.bot, match__result__isnull=False, match__round__competition=sp.competition
+                        bot=self_p.bot, match__result__isnull=False, match__round__competition=self_p.competition
                     )
                     .exclude(match__result__type__in=self._ignored_result_types)
                     .aggregate(Max("resultant_elo"))["resultant_elo__max"]
                 )
-                self.generate_graphs(sp)
-        sp.save()
+                self.generate_graphs(self_p)
+        self_p.save()
 
     @staticmethod
-    def generate_graphs(sp: CompetitionParticipation):
-        graph1, graph2, graph3 = EloGraphsGenerator(sp).generate()
+    def generate_graphs(self_p: CompetitionParticipation):
+        graph1, graph2, graph3 = EloGraphsGenerator(self_p).generate()
         if graph1 is not None:
-            sp.elo_graph.save("elo.png", graph1, False)
+            self_p.elo_graph.save("elo.png", graph1, False)
         if graph2 is not None:
-            sp.elo_graph_update_plot.save("elo_update_plot.png", graph2, False)
+            self_p.elo_graph_update_plot.save("elo_update_plot.png", graph2, False)
         if graph3 is not None:
-            sp.winrate_vs_duration_graph.save("winrate_vs_duration.png", graph3, False)
+            self_p.winrate_vs_duration_graph.save("winrate_vs_duration.png", graph3, False)
 
     @staticmethod
-    def _update_global_statistics(sp: CompetitionParticipation, result: Result):
-        sp.match_count += 1
+    def _update_global_statistics(self_p: CompetitionParticipation, result: Result):
+        self_p.match_count += 1
 
         if result.has_winner:
-            if sp.bot == result.winner:
-                sp.win_count += 1
-                if sp.highest_elo is None or sp.highest_elo < sp.elo:
-                    sp.highest_elo = sp.elo
+            if self_p.bot == result.winner:
+                self_p.win_count += 1
+                if self_p.highest_elo is None or self_p.highest_elo < self_p.elo:
+                    self_p.highest_elo = self_p.elo
             else:
-                sp.loss_count += 1
+                self_p.loss_count += 1
 
-                if MatchParticipation.objects.get(match=result.match, bot=sp.bot).crashed:
-                    sp.crash_count += 1
+                if MatchParticipation.objects.get(match=result.match, bot=self_p.bot).crashed:
+                    self_p.crash_count += 1
 
-                # Special case to match a full recalc:
+                # self_pecial case to match a full recalc:
                 # Set highest_elo isn't already - this will only trigger on a loss or tie
                 # This is easier than changing the way the full recalc determines this figure
-                if sp.highest_elo is None:
-                    sp.highest_elo = sp.elo
+                if self_p.highest_elo is None:
+                    self_p.highest_elo = self_p.elo
 
         elif result.is_tie:
-            sp.tie_count += 1
+            self_p.tie_count += 1
 
-            # Special case to match a full recalc:
+            # self_pecial case to match a full recalc:
             # Set highest_elo isn't already - this will only trigger on a loss or tie
             # This is easier than changing the way the full recalc determines this figure
-            if sp.highest_elo is None:
-                sp.highest_elo = sp.elo
+            if self_p.highest_elo is None:
+                self_p.highest_elo = self_p.elo
         else:
             raise Exception("Unexpected result type: %s", result.type)
 
-        sp.win_perc = sp.win_count / sp.match_count * 100
-        sp.loss_perc = sp.loss_count / sp.match_count * 100
-        sp.tie_perc = sp.tie_count / sp.match_count * 100
-        sp.crash_perc = sp.crash_count / sp.match_count * 100
+        self_p.win_perc = self_p.win_count / self_p.match_count * 100
+        self_p.loss_perc = self_p.loss_count / self_p.match_count * 100
+        self_p.tie_perc = self_p.tie_count / self_p.match_count * 100
+        self_p.crash_perc = self_p.crash_count / self_p.match_count * 100
 
         # TODO: implement caching so that this runs quick enough to include in this job
-        # BotStatistics._generate_graphs(sp)
+        # BotStatistics._generate_graphs(self_p)
 
-        sp.save()
+        self_p.save()
 
     def _recalculate_matchup_stats(self, self_p: CompetitionParticipation):
         CompetitionBotMatchupStats.objects.filter(bot=self_p).delete()
@@ -195,31 +197,31 @@ class BotStatistics:
 
         matchup_stats.save()
 
-    def _recalculate_map_stats(self, sp: CompetitionParticipation):
-        competition_matches = Match.objects.filter(round__competition_id=sp.competition.id).select_related("map")
+    def _recalculate_map_stats(self, self_p: CompetitionParticipation):
+        competition_matches = Match.objects.filter(round__competition_id=self_p.competition.id).select_related("map")
         maps = Map.objects.filter(id__in=competition_matches.values_list("map_id", flat=True))
 
         # purge existing stats entries for all maps
-        CompetitionBotMapStats.objects.filter(bot=sp).delete()
+        CompetitionBotMapStats.objects.filter(bot=self_p).delete()
 
         for map in maps:
-            match_count = self._calculate_map_data(map, sp, self.result_exists)
+            match_count = self._calculate_map_data(map, self_p, self.result_exists)
             if not match_count:
                 continue
 
-            map_stats = CompetitionBotMapStats.objects.create(bot=sp, map=map)
+            map_stats = CompetitionBotMapStats.objects.create(bot=self_p, map=map)
             map_stats.match_count = match_count
 
-            map_stats.win_count = self._calculate_map_data(map, sp, self.result_win)
+            map_stats.win_count = self._calculate_map_data(map, self_p, self.result_win)
             map_stats.win_perc = map_stats.win_count / map_stats.match_count * 100
 
-            map_stats.loss_count = self._calculate_map_data(map, sp, self.result_loss)
+            map_stats.loss_count = self._calculate_map_data(map, self_p, self.result_loss)
             map_stats.loss_perc = map_stats.loss_count / map_stats.match_count * 100
 
-            map_stats.tie_count = self._calculate_map_data(map, sp, self.result_tie)
+            map_stats.tie_count = self._calculate_map_data(map, self_p, self.result_tie)
             map_stats.tie_perc = map_stats.tie_count / map_stats.match_count * 100
 
-            map_stats.crash_count = self._calculate_map_data(map, sp, self.result_crash)
+            map_stats.crash_count = self._calculate_map_data(map, self_p, self.result_crash)
             map_stats.crash_perc = map_stats.crash_count / map_stats.match_count * 100
 
             map_stats.save()
@@ -250,17 +252,17 @@ class BotStatistics:
         map_stats.save()
 
     @staticmethod
-    def _calculate_matchup_data(competition_participation, sp, result_query):
+    def _calculate_matchup_data(opponent_p, self_p, result_query):
         return Match.objects.filter(
-            Exists(MatchParticipation.objects.filter(result_query, match_id=OuterRef("id"), bot_id=sp.bot_id)),
-            Exists(MatchParticipation.objects.filter(match_id=OuterRef("id"), bot_id=competition_participation.bot_id)),
-            round__competition=sp.competition_id,
+            Exists(MatchParticipation.objects.filter(result_query, match_id=OuterRef("id"), bot_id=self_p.bot_id)),
+            Exists(MatchParticipation.objects.filter(match_id=OuterRef("id"), bot_id=opponent_p.bot_id)),
+            round__competition=self_p.competition_id,
         )
 
     @staticmethod
-    def _calculate_map_data(map, sp, result_query):
+    def _calculate_map_data(map, self_p, result_query):
         return Match.objects.filter(
-            Exists(MatchParticipation.objects.filter(result_query, match_id=OuterRef("id"), bot_id=sp.bot_id)),
+            Exists(MatchParticipation.objects.filter(result_query, match_id=OuterRef("id"), bot_id=self_p.bot_id)),
             map_id=map.id,
-            round__competition=sp.competition_id,
+            round__competition=self_p.competition_id,
         ).count()
