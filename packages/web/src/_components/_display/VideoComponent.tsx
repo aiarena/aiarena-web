9a609961df49c46ff22e15041e6ab488c10301c4
DrillableBit <a.a.p.lindberg69@gmail.com>
2024-08-20 21:34:06 +0200

    reloading video on mount, removing dead links in footer

--- packages/web/src/_components/_display/VideoComponent.tsx
+++ packages/web/src/_components/_display/VideoComponent.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const VideoComponent = ({ source }: { source: string }) => {
   const router = useRouter();
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    // Re-initialize the video when the component is mounted or remounted
+    if (videoRef.current) {
+      videoRef.current.load();
+      videoRef.current.play();
+    }
+  }, []);
 
   const handleRedirect = (path: string) => {
     router.push(path);
   };
+
   return (
     <div
       style={{
@@ -18,6 +28,7 @@ const VideoComponent = ({ source }: { source: string }) => {
       }}
     >
       <video
+        ref={videoRef}
         src={source}
         autoPlay
         loop
@@ -38,7 +49,6 @@ const VideoComponent = ({ source }: { source: string }) => {
             "linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 100%)", // For Webkit browsers
         }}
       />
-      {/* Tint overlay with matching fade-out effect */}
       <div
         className="absolute inset-0 bg-black pointer-events-none"
         style={{
@@ -50,38 +60,31 @@ const VideoComponent = ({ source }: { source: string }) => {
             "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 70%, rgba(0, 0, 0, 0) 100%)",
         }}
       ></div>
-
-      {/* Overlay content */}
       <div className="video-overlay absolute inset-0 flex flex-col justify-center items-center text-center text-white px-4">
         <h1 className="text-6xl font-bold mb-8 font-gugi text-customGreen">
-          {" "}
           <Image
             className="mx-auto pb-6"
             src={"/ai-arena-logo.png"}
             alt="AI-arena-logo"
             width={150}
             height={50}
-          ></Image>{" "}
+          />
           AI Arena
         </h1>
         <h2 className="text-2xl mb-8">Welcome to the AI Arena!</h2>
         <div className="flex flex-wrap justify-around w-80">
           <button
             onClick={() => handleRedirect("https://aiarena.net/stream/")}
-            className="
-    hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform "
+            className="hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform"
           >
-            {/* hover:border-4 hover:border-blue-500 */}
             Watch
           </button>
           <button
             onClick={() => handleRedirect("/play")}
-            className="hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform "
+            className="hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform"
           >
             Play
           </button>
-
-         
         </div>
       </div>
     </div>
@@ -90,44 +93,90 @@ const VideoComponent = ({ source }: { source: string }) => {
 
 export default VideoComponent;
 
-// return (
-//   <div style={{ width: '100%', height: '75vh', position: 'relative', overflow: 'hidden' }}>
-//     <video
-//       src={source}
-//       autoPlay
-//       loop
-//       muted
-//       playsInline
-//       style={{
-//         width: '100%',
-//         height: '100%',
-//         objectFit: 'cover',
-//       }}
-//     />
-//     {/* Tint overlay */}
-//     <div className="absolute inset-0 bg-black opacity-20 pointer-events-none"></div>
+// const VideoComponent = ({ source }: { source: string }) => {
+//   const router = useRouter();
 
-//     {/* Overlay content */}
-//     <div className="video-overlay absolute inset-0 flex flex-col justify-center items-center text-center text-white px-4">
-//       <h1 className="text-4xl font-bold mb-8">AI Arena</h1>
-//       <h2 className="text-2xl mb-8">Compete with your AI models</h2>
-//       <div className="flex flex-wrap justify-around w-full">
-//         <button
-//           onClick={() => handleRedirect('/watch')}
-//           className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105 mb-4 md:mb-0"
-//         >
-//           Watch
-//         </button>
-//         <button
-//           onClick={() => handleRedirect('/play')}
-//           className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
-//         >
-//           Play
-//         </button>
+//   const handleRedirect = (path: string) => {
+//     router.push(path);
+//   };
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         height: "85vh",
+//         position: "relative",
+//         overflow: "hidden",
+//       }}
+//     >
+//       <video
+//         src={source}
+//         autoPlay
+//         loop
+//         muted
+//         playsInline
+//         style={{
+//           width: "100%",
+//           height: "100%",
+//           objectFit: "cover",
+//           position: "absolute",
+//           top: 0,
+//           left: 0,
+//           zIndex: 1,
+//           opacity: 1, // Initial opacity
+//           maskImage:
+//             "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 70%, rgba(0, 0, 0, 0) 100%)",
+//           WebkitMaskImage:
+//             "linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 100%)", // For Webkit browsers
+//         }}
+//       />
+//       {/* Tint overlay with matching fade-out effect */}
+//       <div
+//         className="absolute inset-0 bg-black pointer-events-none"
+//         style={{
+//           zIndex: 1,
+//           opacity: 0.6,
+//           maskImage:
+//             "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 70%, rgba(0, 0, 0, 0) 100%)",
+//           WebkitMaskImage:
+//             "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 70%, rgba(0, 0, 0, 0) 100%)",
+//         }}
+//       ></div>
+
+//       {/* Overlay content */}
+//       <div className="video-overlay absolute inset-0 flex flex-col justify-center items-center text-center text-white px-4">
+//         <h1 className="text-6xl font-bold mb-8 font-gugi text-customGreen">
+//           {" "}
+//           <Image
+//             className="mx-auto pb-6"
+//             src={"/ai-arena-logo.png"}
+//             alt="AI-arena-logo"
+//             width={150}
+//             height={50}
+//           ></Image>{" "}
+//           AI Arena
+//         </h1>
+//         <h2 className="text-2xl mb-8">Welcome to the AI Arena!</h2>
+//         <div className="flex flex-wrap justify-around w-80">
+//           <button
+//             onClick={() => handleRedirect("https://aiarena.net/stream/")}
+//             className="
+//     hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform "
+//           >
+//             {/* hover:border-4 hover:border-blue-500 */}
+//             Watch
+//           </button>
+//           <button
+//             onClick={() => handleRedirect("/play")}
+//             className="hover:border-4 border-4 border-customGreen bg-customGreen hover:bg-transparent hover:border-customGreen text-white font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform "
+//           >
+//             Play
+//           </button>
+
+         
+//         </div>
 //       </div>
 //     </div>
-//   </div>
-// );
+//   );
 // };
 
 // export default VideoComponent;
