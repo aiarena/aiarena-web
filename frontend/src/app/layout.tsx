049e92a77744d0c9321cbf7dc56d2d8974168276
DrillableBit <a.a.p.lindberg69@gmail.com>
2024-12-05 00:26:58 +0100

    adding locally hosted font for gugi and quicksand

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,14 +7,16 @@ import {Gugi} from "next/font/google";
 import RelayEnvironment from "@/_lib/RelayEnvironment";
 import {RelayEnvironmentProvider} from "react-relay";
 import {UserProvider} from "@/_components/providers/UserProvider";
+import { quicksand, gugi } from "@/_styles/fonts"; // Adjust the path as necessary
+
 
 const inter = Inter({subsets: ["latin"]});
-const quicksand = Quicksand({
-    subsets: ['latin'],
-    weight: ['300', '400', '500', '600', '700'],
-    variable: '--font-quicksand',
-});
-const gugi = Gugi({subsets: ['latin'], weight: '400', variable: '--font-gugi'});
+// const quicksand = Quicksand({
+//     subsets: ['latin'],
+//     weight: ['300', '400', '500', '600', '700'],
+//     variable: '--font-quicksand',
+// });
+// const gugi = Gugi({subsets: ['latin'], weight: '400', variable: '--font-gugi'});
 
 function RootLayout({children}: { children: React.ReactNode }) {
     return (
