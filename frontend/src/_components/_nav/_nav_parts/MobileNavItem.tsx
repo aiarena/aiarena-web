30da459577ce1e1307ad21ce2717a432a71e1975
DrillableBit <a.a.p.lindberg69@gmail.com>
2024-12-08 02:06:24 +0100

    reverting pathname, removing unneccesary compare

--- frontend/src/_components/_nav/_nav_parts/MobileNavItem.tsx
+++ frontend/src/_components/_nav/_nav_parts/MobileNavItem.tsx
@@ -1,27 +1,19 @@
 "use client";
 import React from "react";
-import { usePathname } from "next/navigation";
+
 import Link from "next/link";
-import { getPublicPrefix } from "@/_lib/getPublicPrefix";
 
 interface NavItemProps {
   href: string;
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }
-function MobileNavItem({ href, children, onClick }: NavItemProps) {
-  const pathname = usePathname();
-
-  const normalizedHref = `${getPublicPrefix()}${href}`.replace(/\/+$/, "");
-  const normalizedPathname = pathname.replace(/\/+$/, "");
-  const isActive = normalizedPathname === normalizedHref;
-
+function MobileNavItem({ href, children, onClick, className }: NavItemProps) {
   return (
     <Link href={href} onClick={onClick}>
       <div
-        className={`cursor-pointer text-xl px-6 py-5 text-center hover:bg-white md:hover:bg-transparent hover:text-black ${
-          isActive ? "border-y-2 border-customGreen" : "border-none"
-        }`}
+        className={`cursor-pointer text-xl px-6 py-5 text-center hover:bg-white md:hover:bg-transparent hover:text-black ${className}`}
       >
         {children}
       </div>
--- frontend/src/_components/_nav/_nav_parts/NavItem.tsx
+++ frontend/src/_components/_nav/_nav_parts/NavItem.tsx
@@ -1,35 +1,20 @@
 "use client";
-import React, { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import React from "react";
 import Link from "next/link";
-import { getPublicPrefix } from "@/_lib/getPublicPrefix";
 
 interface NavItemProps {
   href: string;
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }
 
-function NavItem({ href, children, onClick }: NavItemProps) {
-  const pathname = usePathname();
+function NavItem({ href, children, onClick, className }: NavItemProps) {
 
-  const normalizedHref = `${getPublicPrefix()}${href}`.replace(/\/+$/, "");
-  const normalizedPathname = pathname.replace(/\/+$/, "");
-  const isActive = normalizedPathname === normalizedHref;
-
-  useEffect(() => {
-    console.log("pathname:",pathname)
-    console.log("nomr_path:",normalizedPathname)
-    console.log("nomr_ref:",normalizedHref)
-  }, [normalizedPathname,normalizedHref,pathname])
-  
-  
   return (
     <div className="text-l p-2  text-center">
       <Link
-        className={` hover:text-slate-300 text-white py-2 ${
-          isActive ? "border-b-2 border-customGreen"  : ""
-        }`}
+        className={`py-2 hover:text-slate-300 text-white ${className}`}
         href={href}
         onClick={onClick}
       >
