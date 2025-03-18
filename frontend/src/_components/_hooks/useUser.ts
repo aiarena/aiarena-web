04134bcb50c8d2d74c04a1726f3a4c32c8b7ee75
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-03-18 14:08:17 +0100

    author bots and avatar

--- frontend/src/_components/_hooks/useUser.ts
+++ frontend/src/_components/_hooks/useUser.ts
@@ -5,7 +5,7 @@ import { getDataIDsFromFragment } from 'relay-runtime';
 import { useUserQuery } from './__generated__/useUserQuery.graphql';
 
 export const useUser = (userId: string) => {
-    console.log("attempting to get user with," , userId)
+  console.log("attempting to get user with,", userId)
   const data = useLazyLoadQuery<useUserQuery>(
     graphql`
       query useUserQuery($id: ID!) {
@@ -15,30 +15,54 @@ export const useUser = (userId: string) => {
               username
               patreonLevel
               dateJoined
+              avatarUrl
+              ownBots{
+              edges {
+                node {
+                id    
+              name
+              
+              type
+              playsRace
+            }
+          }}
+
           }
         }
       }
     `,
     { id: userId }
-  );    
+  );
 
 
 
 
-const user = data.node;
-if (!user || !('id' in user)) {
+  const user = data.node;
+  if (!user || !('id' in user)) {
     console.warn(`No user found for ID ${userId}`);
     return null;
   }
 
-const sanitizedBot = {
+  const sanitizedBot = {
     id: user.id,
     dateJoined: String(user.dateJoined), // Ensure string
     username: user.username || "", // Fallback for title
     patreonLevel: user.patreonLevel || "", // Fallback for text
-   
-}
+    avatarUrl: user.avatarUrl || "",
+    ownBots:
+      data.node.ownBots?.edges?.map(edge => ({
+        
+          name: edge?.node?.name || "",
+          type: edge?.node?.type || "",
+          id: edge?.node?.id || "",
+          playsRace: edge?.node?.playsRace || "",
+        
 
-// Transform into a sanitized shape
-return sanitizedBot
+      })) || [],
+
+
+  }
+
+  // Transform into a sanitized shape
+  return sanitizedBot
 }
