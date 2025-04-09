589d75c0e12bd2cf48215df8c29dd0043149ae17
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-04-10 00:52:23 +0200

    Removing statefull relay cache on next server

--- frontend/src/_components/providers/ClientWrapper.tsx
+++ frontend/src/_components/providers/ClientWrapper.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { RelayEnvironmentProvider } from "react-relay";
-import RelayEnvironment from "@/_lib/RelayEnvironment";
+// import { RelayEnvironmentProvider } from "react-relay";
 import { LoginProvider } from "@/_components/providers/LoginProvider";
 import { ViewerProvider } from "@/_components/providers/ViewerProvider";
 import ErrorBoundary from "./ErrorBoundary";
+
+import RelayProvider from "./RelayProvider";
 
 export default function ClientWrapper({
   children,
@@ -14,11 +15,16 @@ export default function ClientWrapper({
   return (
     <div>
       <ErrorBoundary>
-        <RelayEnvironmentProvider environment={RelayEnvironment}>
+        {/* OLD RELAY ENVIORNMENT - SHARED STATE ON SERVER */}
+        {/* <RelayEnvironmentProvider environment={RelayEnvironment}> */}
+
+        {/* New Relay enviornment - Unique env per request on server */}
+        <RelayProvider>
           <LoginProvider>
             <ViewerProvider>{children}</ViewerProvider>
           </LoginProvider>
-        </RelayEnvironmentProvider>
+        </RelayProvider>
+        {/* </RelayEnvironmentProvider> */}
       </ErrorBoundary>
     </div>
   );
