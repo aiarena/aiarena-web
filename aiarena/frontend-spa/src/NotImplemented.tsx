d0d9757ad8bb290fa2f7285f9b7e153a870eea01
Ilia Peterov <ipeterov1@gmail.com>
2025-04-21 02:26:43 +0400

    chore: add working react-relay example

--- aiarena/frontend-spa/src/NotImplemented.tsx
+++ aiarena/frontend-spa/src/NotImplemented.tsx
@@ -1,3 +1,21 @@
+import { graphql, useLazyLoadQuery } from "react-relay";
+import { NotImplementedQuery } from "./__generated__/NotImplementedQuery.graphql";
+
 export default function NotImplemented() {
-  return <span>Not implemented yet</span>;
+  const { viewer } = useLazyLoadQuery<NotImplementedQuery>(
+    graphql`
+      query NotImplementedQuery {
+        viewer {
+          email
+        }
+      }
+    `,
+    {},
+  );
+
+  if (!viewer) {
+    return <span>Not logged in</span>;
+  }
+
+  return <span>Logged in as {viewer.email}</span>;
 }
