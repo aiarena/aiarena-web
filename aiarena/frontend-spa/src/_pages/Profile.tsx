82cc0f727b367ea7408fb6192af305844064fec1
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-04-23 08:05:02 +0200

    Profile Bot overview and profile bot competitions

--- aiarena/frontend-spa/src/_pages/Profile.tsx
+++ aiarena/frontend-spa/src/_pages/Profile.tsx
@@ -1,3 +1,80 @@
+// "use client";
+import TabNavigation from "@/_components/_nav/TabNav";
+import { useState } from "react";
+import { graphql, useLazyLoadQuery } from "react-relay";
+import { ProfileQuery } from "./__generated__/ProfileQuery.graphql";
+import { ProfileBotOverviewList } from "@/_components/_profile/ProfileBotOverviewList";
+// import TabNavigation from "@/_components/_nav/TabNav";
+// import PreFooterSpacer from "@/_components/_display/PreFooterSpacer";
+// import SettingsProfileSection from "@/_components/_display/_profile/SettingsProfileSection";
+// import RequestMatchSection from "@/_components/_display/_profile/RequestMatchSection";
+// import { redirect } from "next/navigation";
+// import { ProfileBotOverviewList } from "@/_components/_display/ProfileBotOverviewList";
+// import { graphql, useLazyLoadQuery } from "react-relay";
+// import { pageProfileDashboardQuery } from "./__generated__/pageProfileDashboardQuery.graphql";
+// import { getPublicPrefix } from "@/_lib/getPublicPrefix";
+
 export default function Profile() {
-  return <span>Profile</span>;
+  const data = useLazyLoadQuery<ProfileQuery>(
+    graphql`
+      query ProfileQuery {
+        viewer {
+          ...ProfileBotOverviewList_viewer
+          # ...RequestMatchSection_viewer
+          # ...SettingsProfileSection_viewer
+        }
+      }
+    `,
+    {}
+  );
+
+  const [activeTab, setActiveTab] = useState("Bots");
+  if (!data.viewer) {
+    return (
+      <>
+        <p>No viewer</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className=" flex justify-center text-gray-300">
+        <div className="mx-auto py-8 px-4 max-w-7xl bg-gray-800 w-full">
+          {/* Navigation Tabs */}
+          <TabNavigation
+            tabs={[
+              { name: "Bots", href: "#bot-overview" },
+              { name: "Requested Matches", href: "#matches" },
+              { name: "Settings", href: "#settings" },
+            ]}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+
+          {/* Content Sections */}
+          <div className="mt-8">
+            {activeTab === "Bots" && (
+              <div id="bot-overview">
+                <ProfileBotOverviewList viewer={data.viewer} />
+              </div>
+            )}
+
+            {/* {activeTab === "Requested Matches" && (
+              <div id="matches">
+                <RequestMatchSection viewer={data.viewer} />
+              </div>
+            )} */}
+
+            {/* {data && activeTab === "Settings" && (
+              <div id="settigns">
+                <SettingsProfileSection viewer={data.viewer} />
+              </div>
+            )} */}
+          </div>
+        </div>
+      </div>
+      {/* <PreFooterSpacer /> */}
+    </>
+  );
 }
