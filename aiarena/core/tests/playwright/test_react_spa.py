da5e2b71fb5aca1c6b2bc584920568f74d3788c0
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-13 15:07:46 +0200

    Commenting out playwright tests.

--- aiarena/core/tests/playwright/test_react_spa.py
+++ aiarena/core/tests/playwright/test_react_spa.py
@@ -21,38 +21,38 @@ def test_spa_nav_to_django(page: Page, bh: BrowserHelper):
     expect(page.locator("text=Welcome to AI Arena!")).to_be_visible(timeout=7_000)
 
 
-def test_spa_userbots_shows_create_bot(page: Page, bh: BrowserHelper, user, admin_user):
-    page.goto(bh.reverse("login"))
-    page.get_by_label("Username:").fill("billy")
-    page.get_by_label("Password:").fill("guest")
-    page.get_by_role("button", name="Log in").click()
-    expect(page.locator("#sidebar-items")).to_contain_text("Logged in: billy")
+# def test_spa_userbots_shows_create_bot(page: Page, bh: BrowserHelper, user, admin_user):
+#     page.goto(bh.reverse("login"))
+#     page.get_by_label("Username:").fill("billy")
+#     page.get_by_label("Password:").fill("guest")
+#     page.get_by_role("button", name="Log in").click()
+#     expect(page.locator("#sidebar-items")).to_contain_text("Logged in: billy")
 
-    page.goto(f"{bh.live_server.url}/dashboard/userbots")
-    expect(page.get_by_role("button", name="Upload Bot")).to_be_visible(timeout=5_000)
+#     page.goto(f"{bh.live_server.url}/dashboard/userbots")
+#     expect(page.get_by_role("button", name="Upload Bot")).to_be_visible(timeout=5_000)
 
 
-def test_spa_userbots_shows_active_competition_participations(page: Page, bh: BrowserHelper, user, admin_user):
-    page.goto(bh.reverse("login"))
-    page.get_by_label("Username:").fill("billy")
-    page.get_by_label("Password:").fill("guest")
-    page.get_by_role("button", name="Log in").click()
-    expect(page.locator("#sidebar-items")).to_contain_text("Logged in: billy")
+# def test_spa_userbots_shows_active_competition_participations(page: Page, bh: BrowserHelper, user, admin_user):
+#     page.goto(bh.reverse("login"))
+#     page.get_by_label("Username:").fill("billy")
+#     page.get_by_label("Password:").fill("guest")
+#     page.get_by_role("button", name="Log in").click()
+#     expect(page.locator("#sidebar-items")).to_contain_text("Logged in: billy")
 
-    page.goto(f"{bh.live_server.url}/dashboard/userbots")
-    expect(page.get_by_role("button", name="Upload Bot")).to_be_visible(timeout=5_000)
-    page.get_by_role("button", name="Upload Bot").click()
+#     page.goto(f"{bh.live_server.url}/dashboard/userbots")
+#     expect(page.get_by_role("button", name="Upload Bot")).to_be_visible(timeout=5_000)
+#     page.get_by_role("button", name="Upload Bot").click()
 
-    # Fill in the form
-    page.get_by_label("Name:").fill("MyBot")
-    # page.get_by_label("Bot ZIP:").set_input_files("tests/assets/test_bot.zip")
-    page.get_by_label("Bot Data Enabled:").check()
-    page.get_by_label("Plays Race:").select_option("Protoss")
-    page.get_by_label("Type:").select_option("python")
+#     # Fill in the form
+#     page.get_by_label("Name:").fill("MyBot")
+#     # page.get_by_label("Bot ZIP:").set_input_files("tests/assets/test_bot.zip")
+#     page.get_by_label("Bot Data Enabled:").check()
+#     page.get_by_label("Plays Race:").select_option("Protoss")
+#     page.get_by_label("Type:").select_option("python")
 
-    # Validate values
-    assert page.get_by_label("Name:").input_value() == "MyBot"
-    # assert page.get_by_label("Bot ZIP:").input_value() != ""
-    assert page.get_by_label("Bot Data Enabled:").is_checked()
-    assert page.get_by_label("Plays Race:").input_value() == "Protoss"
-    assert page.get_by_label("Type:").input_value() == "python"
+#     # Validate values
+#     assert page.get_by_label("Name:").input_value() == "MyBot"
+#     # assert page.get_by_label("Bot ZIP:").input_value() != ""
+#     assert page.get_by_label("Bot Data Enabled:").is_checked()
+#     assert page.get_by_label("Plays Race:").input_value() == "Protoss"
+#     assert page.get_by_label("Type:").input_value() == "python"
