c02043bb73fd996a0b1db751a80cab5a079a2caf
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-17 18:28:10 +0200

    BotPage: Sorting/ Search / Dropdown prop

--- aiarena/frontend-spa/src/_components/_sections/ProfileBotOverviewList.tsx
+++ aiarena/frontend-spa/src/_components/_sections/ProfileBotOverviewList.tsx
@@ -7,6 +7,9 @@ import MainButton from "../_props/MainButton";
 import UploadBotModal from "./_modals/UploadBotModal";
 import ProfileBot from "./ProfileBot";
 import { ProfileBotOverviewList_user$key } from "./__generated__/ProfileBotOverviewList_user.graphql";
+import Searchbar from "../_props/Searchbar";
+import Dropdown from "../_props/Dropdown";
+import DropdownButton from "../_props/DropdownButton";
 
 interface ProfileBotOverviewListProps {
   viewer: ProfileBotOverviewList_viewer$key;
@@ -34,6 +37,16 @@ export const ProfileBotOverviewList: React.FC<ProfileBotOverviewListProps> = (
           edges {
             node {
               id
+              botZipUpdated
+              created
+              name
+              trophies {
+                edges {
+                  node {
+                    id
+                  }
+                }
+              }
               competitionParticipations {
                 edges {
                   node {
@@ -63,10 +76,13 @@ export const ProfileBotOverviewList: React.FC<ProfileBotOverviewListProps> = (
     0
   );
 
+  const [useSort, setUseSort] = useState("Sort By");
+  const [searchBarValue, setSearchBarValue] = useState("");
+
   return (
     <div className="bg-customBackgroundColor1">
-      <div className="flex justify-between ">
-        <div className="flex gap-2 pb-2 mt-auto flex-wrap">
+      <div className="flex justify-between  flex-wrap-reverse w-full pb-4">
+        <div className="flex gap-2 flex-wrap">
           {viewer.activeBotsLimit ? (
             <div className="block">
               <p className="pb-1">
@@ -86,26 +102,96 @@ export const ProfileBotOverviewList: React.FC<ProfileBotOverviewListProps> = (
             </div>
           ) : null}
         </div>
-        {/* <h2 className="text-2xl font-bold text-customGreen mb-4">Your Bots</h2> */}
-        <div className="pb-4">
-          <div className="hidden md:block">
-            <MainButton
-              onClick={() => setUploadBotModalOpen(true)}
-              text="Upload Bot"
+
+        <div className="flex gap-4 ">
+          <Dropdown title={useSort}>
+            <DropdownButton
+              onClick={() => setUseSort("Active Competitions")}
+              title={"Active Competitions"}
             />
-          </div>
-          <div className="block md:hidden">
-            <MainButton onClick={() => setUploadBotModalOpen(true)} text="+" />
+            <DropdownButton
+              onClick={() => setUseSort("Zip Updated")}
+              title={"Zip Updated"}
+            />
+            <DropdownButton
+              onClick={() => setUseSort("Created")}
+              title={"Created"}
+            />
+            <DropdownButton
+              onClick={() => setUseSort("Trophies")}
+              title={"Trophies"}
+            />
+          </Dropdown>
+
+          <Searchbar
+            onChange={(e) => {
+              setSearchBarValue(e.target.value);
+              setUseSort("Sort By");
+            }}
+            value={searchBarValue}
+            placeholder="Search your bots..."
+          />
+
+          <div>
+            <div className="hidden md:block">
+              <MainButton
+                onClick={() => setUploadBotModalOpen(true)}
+                text="Upload Bot"
+              />
+            </div>
+            <div className="block md:hidden">
+              <MainButton
+                onClick={() => setUploadBotModalOpen(true)}
+                text="+"
+              />
+            </div>
           </div>
         </div>
       </div>
 
       <ul className="space-y-12">
-        {getNodes(userData?.ownBots).map((bot) => (
-          <li key={bot.id}>
-            <ProfileBot bot={bot} />
-          </li>
-        ))}
+        {getNodes(userData?.ownBots)
+          .sort((a, b) => {
+            switch (useSort) {
+              case "Zip Updated":
+                return a.botZipUpdated <= b.botZipUpdated ? 0 : -1;
+              case "Created":
+                return a.created <= b.created ? 0 : -1;
+              case "Trophies":
+                return (a.trophies?.edges?.length || 0) <=
+                  (b.trophies?.edges?.length || 0)
+                  ? 0
+                  : -1;
+              case "Active Competitions":
+                return (a.competitionParticipations?.edges?.length || 0) <=
+                  (b.competitionParticipations?.edges?.length || 0)
+                  ? 0
+                  : -1;
+
+              default:
+                if (searchBarValue.trim().length > 0) {
+                  const pattern = searchBarValue?.trim();
+
+                  const regex = new RegExp(pattern, "i");
+                  const aMatches = regex.test(a.name) ? 0 : -1;
+                  const bMatches = regex.test(b.name) ? 0 : -1;
+
+                  return bMatches - aMatches;
+                } else if (activeBotParticipation == 0) {
+                  return a.botZipUpdated <= b.botZipUpdated ? 0 : -1;
+                } else {
+                  return (a.competitionParticipations?.edges?.length || 0) <=
+                    (b.competitionParticipations?.edges?.length || 0)
+                    ? 0
+                    : -1;
+                }
+            }
+          })
+          .map((bot) => (
+            <li key={bot.id}>
+              <ProfileBot bot={bot} />
+            </li>
+          ))}
       </ul>
       <UploadBotModal
         isOpen={isUploadBotModalOpen}
