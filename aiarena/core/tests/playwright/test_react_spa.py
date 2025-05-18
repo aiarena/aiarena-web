7b4a0165cef7cba18af1c9c1e2964dcecf02bf88
Ilia Peterov <ipeterov1@gmail.com>
2025-05-18 20:46:13 +0400

    Implement file upload for test_spa_userbots_shows_active_competition_participations

--- aiarena/core/tests/playwright/test_react_spa.py
+++ aiarena/core/tests/playwright/test_react_spa.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Page, expect
+from playwright.sync_api import FilePayload, Page, expect
 
 from ..base import BrowserHelper
 
@@ -31,8 +31,14 @@ def test_spa_userbots_shows_create_bot(page: Page, bh: BrowserHelper, user, admi
     expect(page.get_by_role("button", name="Upload Bot")).to_be_visible()
 
 
-@pytest.mark.skip(reason="to be implemented once we implement mock files")
-def test_spa_userbots_shows_active_competition_participations(page: Page, bh: BrowserHelper, user, admin_user):
+def test_spa_userbots_shows_active_competition_participations(
+    page: Page,
+    bh: BrowserHelper,
+    user,
+    admin_user,
+    python_zip_file,
+    all_bot_races,
+):
     page.goto(bh.reverse("login"))
     page.get_by_label("Username:").fill("billy")
     page.get_by_label("Password:").fill("guest")
@@ -44,26 +50,27 @@ def test_spa_userbots_shows_active_competition_participations(page: Page, bh: Br
     page.get_by_role("button", name="Upload Bot").click()
 
     # Fill in the form
-    page.get_by_label("Name:").fill("MyBot")
-    page.get_by_label("Bot ZIP:").set_input_files("./assets/test_bot_python.zip")
+    page.get_by_label("Name:").fill("robo-her0")
+    expect(page.get_by_label("Name:")).to_have_value("robo-her0")
+
+    page.get_by_label("Bot ZIP:").set_input_files(
+        FilePayload(
+            name=python_zip_file.name,
+            mimeType=python_zip_file.content_type,
+            buffer=python_zip_file.read(),
+        )
+    )
     page.get_by_label("Bot Data Enabled:").check()
-    page.get_by_label("Plays Race:").select_option("Protoss")
-    page.get_by_label("Type:").select_option("python")
-
-    expect(page.get_by_label("Name:")).to_have_value("MyBot")
-
     expect(page.get_by_label("Bot Data Enabled:")).to_be_checked()
 
     page.get_by_label("Plays Race:").select_option("Protoss")
     expect(page.get_by_label("Plays Race:")).to_have_value("P")
-    page.get_by_label("Plays Race:").select_option("Protoss")
-    expect(page.get_by_label("Plays Race:")).to_have_value("P")
-    page.get_by_label("Plays Race:").select_option("Protoss")
 
     page.get_by_label("Type:").select_option("python")
     expect(page.get_by_label("Type:")).to_have_value("python")
+
     page.locator("form").get_by_role("button", name="Upload Bot").click()
 
     expect(page.locator("form").get_by_role("button", name="Upload Bot")).not_to_be_visible()
-    expect(page.locator("text=MyBot")).to_be_visible()
-    # expect snackbar text
+    expect(page.get_by_text("Bot Uploaded Successfully!")).to_be_visible()
+    expect(page.get_by_text("robo-her0")).to_be_visible()
