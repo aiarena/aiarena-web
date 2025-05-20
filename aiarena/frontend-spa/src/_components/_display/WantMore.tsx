03f3a561febeeed5a512f11c1851cbd55942f54d
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-20 18:05:40 +0200

    Component: Abstracting | Want More ? - donate | into a component

---
+++ aiarena/frontend-spa/src/_components/_display/WantMore.tsx
@@ -0,0 +1,13 @@
+export default function WantMore() {
+  return (
+    <p>
+      <i>
+        Want more? Consider{" "}
+        <a className="cursor-pointer" href={"https://www.patreon.com/aiarena"}>
+          supporting us
+        </a>
+        .
+      </i>
+    </p>
+  );
+}
