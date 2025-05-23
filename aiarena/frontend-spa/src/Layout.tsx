1d611243d6fce088276636a148fbb3a3dd0ad854
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-23 19:56:29 +0200

    Refactor: moving scroll to utils function

--- aiarena/frontend-spa/src/Layout.tsx
+++ aiarena/frontend-spa/src/Layout.tsx
@@ -4,23 +4,7 @@ import Footer from "./_components/_nav/Footer";
 import BackgroundTexture from "./_components/_display/BackgroundTexture";
 import WithSideNav from "./_components/_nav/WithSideNav";
 import { useEffect } from "react";
-
-function scrollToHashWithOffset(offset = -40) {
-  if (!window.location.hash) return;
-
-  const id = window.location.hash.slice(1);
-  const el = document.getElementById(id);
-  if (!el) return;
-
-  const rect = el.getBoundingClientRect();
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  const top = rect.top + scrollTop + offset;
-
-  window.scrollTo({
-    top,
-    behavior: "smooth",
-  });
-}
+import { scrollToHashWithOffset } from "./_lib/layoutUtils";
 
 export default function Layout() {
   const { pathname, hash } = useLocation();
