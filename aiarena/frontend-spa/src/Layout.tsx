a34e40fa84b6949e5a1a89bc56a8c763203601ee
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-23 18:42:02 +0200

    Layout: Scroll to top on nav if no hash

--- aiarena/frontend-spa/src/Layout.tsx
+++ aiarena/frontend-spa/src/Layout.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "./_components/_nav/Navbar";
 import Footer from "./_components/_nav/Footer";
 import BackgroundTexture from "./_components/_display/BackgroundTexture";
 import WithSideNav from "./_components/_nav/WithSideNav";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
+
   return (
     <BackgroundTexture>
       <div className="flex flex-col min-h-screen font-quicksand ">
