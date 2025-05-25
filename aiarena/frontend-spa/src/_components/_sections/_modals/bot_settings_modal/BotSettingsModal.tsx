06b594fa3f16ddbfbea9148dff42f40a318799ae
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-25 15:36:34 +0200

    BotSettingsModal: UI design

--- aiarena/frontend-spa/src/_components/_sections/_modals/bot_settings_modal/BotSettingsModal.tsx
+++ aiarena/frontend-spa/src/_components/_sections/_modals/bot_settings_modal/BotSettingsModal.tsx
@@ -89,10 +89,42 @@ export default function BotSettingsModal({
             onClick={() => setBiographyModalOpen(true)}
             title="Edit Bot Biography"
           />
-          <div className="items-center mt-2">
-            <div className=" mt-2">
-              <label className="text-gray-300 flex items-center">
-                Bot Zip Publicly Downloadable:
+          <SectionDivider color="gray" className="pb-1" />
+
+          <div className="flex items-center">
+            <label className="text-gray-300 flex items-center">
+              Store Bot Data:
+              <input
+                type="checkbox"
+                checked={bot.botDataEnabled}
+                onChange={() =>
+                  updateBot({
+                    variables: {
+                      input: {
+                        id: bot.id,
+                        botDataEnabled: !bot.botDataEnabled,
+                      },
+                    },
+                    onCompleted: (...args) => {
+                      onCompleted(...args);
+                    },
+                    onError,
+                  })
+                }
+                className="ml-2 w-5 h-5"
+                disabled={updating}
+              />
+            </label>
+          </div>
+          <SectionDivider color="gray" className="pb-4" />
+          <div className="pb-4 flex items-center justify-between gap-8 flex-wrap items-start ">
+            {/* Bot Data & Bot Zip */}
+
+            <div className="w-full md:w-[45%]">
+              <div className="flex items-center mt-2">
+                <label className="text-gray-300 flex items-center">
+                  Bot Zip Publicly Downloadable:
+                </label>
                 <input
                   type="checkbox"
                   checked={bot.botZipPubliclyDownloadable}
@@ -110,37 +142,14 @@ export default function BotSettingsModal({
                   disabled={updating}
                   className="ml-2 w-5 h-5"
                 />
-              </label>
+              </div>
             </div>
-
-            <div className="flex items-center mt-2">
-              <label className="text-gray-300 flex items-center">
-                Store Bot Data:
-                <input
-                  type="checkbox"
-                  checked={bot.botDataEnabled}
-                  onChange={() =>
-                    updateBot({
-                      variables: {
-                        input: {
-                          id: bot.id,
-                          botDataEnabled: !bot.botDataEnabled,
-                        },
-                      },
-                      onCompleted: (...args) => {
-                        onCompleted(...args);
-                      },
-                      onError,
-                    })
-                  }
-                  className="ml-2 w-5 h-5"
-                  disabled={updating}
-                />
-              </label>
-            </div>
-            <div className=" mt-2">
-              <label className="text-gray-300 flex items-center">
-                Bot Data Publicly Downloadable:
+            <div className="w-full md:w-[45%]">
+              {" "}
+              <div className="flex items-center mt-2">
+                <label className="text-gray-300 flex items-center">
+                  Bot Data Publicly Downloadable:
+                </label>
                 <input
                   type="checkbox"
                   checked={bot.botDataPubliclyDownloadable}
@@ -158,135 +167,135 @@ export default function BotSettingsModal({
                   disabled={updating}
                   className="ml-2 w-5 h-5"
                 />
-              </label>
+              </div>
             </div>
           </div>
-          <SectionDivider color="gray" />
-          {/* Bot Data & Bot Zip */}
-          <div className="pt-4">
-            {/* Bot Zip */}
-            <div className="pb-4 flex items-center justify-between gap-8 flex-wrap">
-              <div className="min-w-[40%]">
-                <div className="flex items-center  gap-2  pb-4">
-                  <h3 className="text-xl font-bold text-customGreen-light flex">
-                    <FolderOpenIcon className="size-5 m-1" />
-                    Bot Zip
-                  </h3>
-                  <a
-                    href="/wiki/bot-development/getting-started/#wiki-toc-bot-zip"
-                    target="_blank"
-                    className="pt-1"
-                  >
-                    Wiki
-                  </a>
-                </div>
+          <SectionDivider color="gray" className="pb-4" />
 
-                <SquareButton
-                  disabled={bot.botZip == "{}"}
-                  onClick={() => handleDownload(bot.botZip)}
-                  outerClassName="w-full mb-8"
+          <div className="pb-4 flex items-center justify-between gap-8 flex-wrap items-start ">
+            {/* Bot Data & Bot Zip */}
+
+            <div className="w-full md:w-[40%]">
+              {/* Bot Zip */}
+              <div className="flex items-center  gap-2  pb-4">
+                <h3 className="text-xl font-bold text-customGreen-light flex">
+                  <FolderOpenIcon className="size-5 m-1" />
+                  Bot Zip
+                </h3>
+                <a
+                  href="/wiki/bot-development/getting-started/#wiki-toc-bot-zip"
+                  target="_blank"
+                  className="pt-1"
                 >
-                  <ArrowDownOnSquareIcon
-                    className="size-6 mr-2"
-                    title="Download ZIP"
-                  />
-                  Download Bot Zip
-                </SquareButton>
-
-                <UploadFile
-                  accept=".zip"
-                  file={botZipFile}
-                  setFile={setBotZipFile}
-                  id={"bot-zip-file-upload"}
-                />
-
-                <SquareButton
-                  disabled={!botZipFile}
-                  onClick={() => {
-                    if (!botZipFile) return;
-                    updateBot({
-                      variables: {
-                        input: { id: bot.id, botZip: null },
-                      },
-                      uploadables: {
-                        "input.botZip": botZipFile,
-                      },
-                      onCompleted: (...args) => {
-                        const success = onCompleted(...args);
-                        if (success) setBotZipFile(null);
-                        onCompleted(...args);
-                      },
-                      onError,
-                    });
-                  }}
-                  outerClassName="w-full mt-4"
-                >
-                  <ArrowUpOnSquareIcon className="size-5 mr-1" />
-                  Upload Bot Zip
-                </SquareButton>
+                  Wiki
+                </a>
               </div>
 
-              {/* Bot Data */}
-              <div className="min-w-[40%]">
-                <div className="flex items-center gap-2  pb-4">
-                  <h3 className="text-xl font-bold text-customGreen-light flex">
-                    <FolderOpenIcon className="size-5 m-1" />
-                    Bot Data
-                  </h3>
-                  <a
-                    href="/wiki/bot-development/#wiki-toc-bot-memory-the-data-folder"
-                    target="_blank"
-                    className="pt-1"
-                  >
-                    Wiki
-                  </a>
-                </div>
-
-                <SquareButton
-                  disabled={!bot.botData}
-                  onClick={() =>
-                    bot.botData ? handleDownload(bot.botData) : null
-                  }
-                  outerClassName="w-full mb-8"
-                >
-                  <ArrowDownOnSquareStackIcon
-                    className="size-6 mr-1"
-                    title="Download Data"
-                  />
-                  Download Bot Data
-                </SquareButton>
-
-                <UploadFile
-                  accept=".zip"
-                  file={botDataFile}
-                  setFile={setBotDataFile}
-                  id={"bot-data-file-upload"}
+              <SquareButton
+                disabled={bot.botZip == "{}"}
+                onClick={() => handleDownload(bot.botZip)}
+                outerClassName="w-full mb-8"
+              >
+                <ArrowDownOnSquareIcon
+                  className="size-6 mr-2"
+                  title="Download ZIP"
                 />
-                <SquareButton
-                  disabled={!botDataFile}
-                  onClick={() => {
-                    if (!botDataFile) return;
-                    updateBot({
-                      variables: {
-                        input: { id: bot.id, botData: null },
-                      },
-                      uploadables: {
-                        "input.botData": botDataFile,
-                      },
-                      onCompleted: (...args) => {
-                        const success = onCompleted(...args);
-                        if (success) setBotDataFile(null);
-                        onCompleted(...args);
-                      },
-                      onError,
-                    });
-                  }}
-                  outerClassName="w-full mt-4"
+                Download Bot Zip
+              </SquareButton>
+
+              <UploadFile
+                accept=".zip"
+                file={botZipFile}
+                setFile={setBotZipFile}
+                id={"bot-zip-file-upload"}
+              />
+
+              <SquareButton
+                disabled={!botZipFile}
+                onClick={() => {
+                  if (!botZipFile) return;
+                  updateBot({
+                    variables: {
+                      input: { id: bot.id, botZip: null },
+                    },
+                    uploadables: {
+                      "input.botZip": botZipFile,
+                    },
+                    onCompleted: (...args) => {
+                      const success = onCompleted(...args);
+                      if (success) setBotZipFile(null);
+                      onCompleted(...args);
+                    },
+                    onError,
+                  });
+                }}
+                outerClassName="w-full mt-4"
+              >
+                <ArrowUpOnSquareIcon className="size-5 mr-1" />
+                Upload Bot Zip
+              </SquareButton>
+            </div>
+
+            {/* Bot Data */}
+            <div className="w-full md:w-[40%]">
+              <div className="flex items-center gap-2  pb-4">
+                <h3 className="text-xl font-bold text-customGreen-light flex">
+                  <FolderOpenIcon className="size-5 m-1" />
+                  Bot Data
+                </h3>
+                <a
+                  href="/wiki/bot-development/#wiki-toc-bot-memory-the-data-folder"
+                  target="_blank"
+                  className="pt-1"
                 >
-                  <ArrowUpOnSquareStackIcon className="size-5 mr-1" />
-                  Upload Bot Data
-                </SquareButton>
+                  Wiki
+                </a>
               </div>
+
+              <SquareButton
+                disabled={!bot.botData}
+                onClick={() =>
+                  bot.botData ? handleDownload(bot.botData) : null
+                }
+                outerClassName="w-full mb-8"
+              >
+                <ArrowDownOnSquareStackIcon
+                  className="size-6 mr-1"
+                  title="Download Data"
+                />
+                Download Bot Data
+              </SquareButton>
+
+              <UploadFile
+                accept=".zip"
+                file={botDataFile}
+                setFile={setBotDataFile}
+                id={"bot-data-file-upload"}
+              />
+              <SquareButton
+                disabled={!botDataFile}
+                onClick={() => {
+                  if (!botDataFile) return;
+                  updateBot({
+                    variables: {
+                      input: { id: bot.id, botData: null },
+                    },
+                    uploadables: {
+                      "input.botData": botDataFile,
+                    },
+                    onCompleted: (...args) => {
+                      const success = onCompleted(...args);
+                      if (success) setBotDataFile(null);
+                      onCompleted(...args);
+                    },
+                    onError,
+                  });
+                }}
+                outerClassName="w-full mt-4"
+              >
+                <ArrowUpOnSquareStackIcon className="size-5 mr-1" />
+                Upload Bot Data
+              </SquareButton>
             </div>
           </div>
         </div>
