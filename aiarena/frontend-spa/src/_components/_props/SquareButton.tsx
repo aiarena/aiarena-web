369dc92914e48e4c8fc570ca60bb4d0cf1e17cfa
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-25 14:02:55 +0200

    Frontend:SquareBtn, enable disable, smaller border

--- aiarena/frontend-spa/src/_components/_props/SquareButton.tsx
+++ aiarena/frontend-spa/src/_components/_props/SquareButton.tsx
@@ -71,8 +71,8 @@ export default function SquareButton({
       <div className="relative inline-block">
         <button
           onClick={handleClick}
-          className={`${className} shadow-sm shadow-black hover:shadow-customGreen border-2 border-customGreen bg-darken-2 hover:border-customGreen hover:bg-neutral-800 text-white font-semibold py-1 px-2 rounded-sm transition duration-300 ease-in-out transform`}
-          //   disabled={isLoading}
+          className={`${className} shadow-sm shadow-black hover:shadow-customGreen border-1 border-customGreen bg-darken-2 hover:border-customGreen hover:bg-neutral-800 text-white font-semibold py-1 px-2 rounded-sm transition duration-300 ease-in-out transform`}
+          disabled={isLoading}
         >
           {text}
         </button>
