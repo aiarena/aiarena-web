683599cf07e40606a1b3e45b84f503d13d8b2387
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-05-26 17:56:12 +0200

    Frontend(dateUtils): handle edgecases

--- aiarena/frontend-spa/src/_lib/dateUtils.ts
+++ aiarena/frontend-spa/src/_lib/dateUtils.ts
@@ -1,14 +1,30 @@
 // For displaying data to users
 export const formatDate = (dateString: string, locale = "en-GB") => {
+  if (!dateString) {
+    return ""
+  }
+
+  if (dateString.trim() == "") {
+    return ""
+  }
+
   try {
     return new Date(dateString).toLocaleDateString(locale) || "";
   } catch {
     return "";
   }
-};
+}
 
 // For internal uses
 export const formatDateISO = (dateString: string): string => {
+  if (!dateString) {
+    return ""
+  }
+
+  if (dateString.trim() == "") {
+    return ""
+  }
+
   try {
     const date = new Date(dateString);
     return date.toISOString().replace("T", " ").slice(0, 19) || ""; // YYYY-MM-DD HH:mm:ss
