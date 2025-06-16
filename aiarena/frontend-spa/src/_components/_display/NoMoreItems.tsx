16b5626f9ed5fde5a1b02a64a4b926e1b65f0891
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-06-16 14:34:31 +0200

    Frontend(NoMoreItemsComponent)

---
+++ aiarena/frontend-spa/src/_components/_display/NoMoreItems.tsx
@@ -0,0 +1,9 @@
+import SectionDivider from "./SectionDivider";
+
+export default function NoMoreItems() {
+  return (
+    <div className="flex justify-center ">
+      <SectionDivider color="gray" className="max-w-[75%]" />
+    </div>
+  );
+}
