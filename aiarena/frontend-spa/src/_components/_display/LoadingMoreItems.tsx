35c5aa7e234898ea9f4dd971e079e2fa69338b29
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-06-30 21:42:05 +0200

    Frontend(LoadingMoreItems): huge white spinner instead of dots

--- aiarena/frontend-spa/src/_components/_display/LoadingMoreItems.tsx
+++ aiarena/frontend-spa/src/_components/_display/LoadingMoreItems.tsx
@@ -1,4 +1,4 @@
-import LoadingDots from "./LoadingDots";
+import LoadingSpinner from "./LoadingSpinnerGray";
 
 type LoadingMoreItemsProps = {
   loadingMessage?: string;
@@ -8,11 +8,11 @@ export default function LoadingMoreItems({
   loadingMessage = "Loading More items...",
 }: LoadingMoreItemsProps) {
   return (
-    <div className="p-4">
+    <div className="p-4 w-full flex flex-col items-center">
+      <LoadingSpinner color="white" height="h-20" width="w-20" />
       <p className="text-gray-300 p-3">
         <i>{loadingMessage}</i>
       </p>
-      <LoadingDots />
     </div>
   );
 }
--- aiarena/frontend-spa/src/_components/_display/LoadingSpinnerGray.tsx
+++ aiarena/frontend-spa/src/_components/_display/LoadingSpinnerGray.tsx
@@ -2,17 +2,21 @@ import clsx from "clsx";
 
 interface LoadingSpinnerProps {
   color?: "gray" | "light-gray" | "white" | "black";
+  height?: string;
+  width?: string;
 }
 
 export default function LoadingSpinner({
   color = "gray",
+  height = "h-5",
+  width = "w-5",
 }: LoadingSpinnerProps) {
   return (
     <div
       className={clsx(
         "animate-spin",
-        "h-5",
-        "w-5",
+        height,
+        width,
         "border-2",
         "border-t-transparent",
         "rounded-full",
