5bb808d7ef6f73d56c0100579189cf90a090b7f1
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-10-26 14:03:53 +0100

    Revert "Add competitions page"
    This reverts commit ecdb10f76754006b592cf417382ac933e2585668.

--- aiarena/frontend-spa/src/_pages/Rework/Competitions/Page.tsx
+++ aiarena/frontend-spa/src/_pages/Rework/Competitions/Page.tsx
@@ -1,11 +1,3 @@
-import LoadingSpinner from "@/_components/_display/LoadingSpinnerGray";
-import { Suspense } from "react";
-import Competitions from "./Competitions";
-
-export default function AuthorsPage() {
-  return (
-    <Suspense fallback={<LoadingSpinner color="light-gray" />}>
-      <Competitions />
-    </Suspense>
-  );
+export default function CompetitionsPage() {
+  return <div>Page</div>;
 }
