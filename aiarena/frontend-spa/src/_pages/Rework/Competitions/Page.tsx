e2e49e89b16769dc9a5e34577d06742aabed5718
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-11-16 19:17:59 +0100

    Rename Competitionpage

--- aiarena/frontend-spa/src/_pages/Rework/Competitions/Page.tsx
+++ aiarena/frontend-spa/src/_pages/Rework/Competitions/Page.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from "@/_components/_display/LoadingSpinnerGray";
 import { Suspense } from "react";
 import Competitions from "./Competitions";
 
-export default function AuthorsPage() {
+export default function CompetitionsPage() {
   return (
     <Suspense fallback={<LoadingSpinner color="light-gray" />}>
       <Competitions />
