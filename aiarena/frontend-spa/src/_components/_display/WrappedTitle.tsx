f02df53207bea8ff1601b1a9af6f501416837f37
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-11-16 19:16:16 +0100

    Variable font on wrappedtitle

--- aiarena/frontend-spa/src/_components/_display/WrappedTitle.tsx
+++ aiarena/frontend-spa/src/_components/_display/WrappedTitle.tsx
@@ -1,9 +1,17 @@
-export default function WrappedTitle({ title }: { title: string }) {
+import clsx from "clsx";
+
+export default function WrappedTitle({
+  title,
+  font = "font-gugi font-light",
+}: {
+  title: string;
+  font?: string;
+}) {
   return (
     <>
       <div className="flex items-center mb-4 justify-center relative">
         <div className="flex-1 h-[2px] bg-gradient-to-l from-customGreen to-transparent"></div>
-        <h2 className="text-2xl p-2 font-gugi font-light">{title}</h2>
+        <h2 className={clsx("text-2xl p-2", font)}>{title}</h2>
         <div className="flex-1 h-[2px] bg-gradient-to-r from-customGreen to-transparent"></div>
       </div>
     </>
