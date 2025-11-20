6bef1d064d97eac8c4341797751497cf158b7537
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-11-20 17:27:47 +0100

    Remove react import

--- aiarena/frontend-spa/src/_pages/Rework/_Match/MatchInfo.tsx
+++ aiarena/frontend-spa/src/_pages/Rework/_Match/MatchInfo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { graphql, useFragment } from "react-relay";
 import { MatchInfo_match$key } from "./__generated__/MatchInfo_match.graphql";
 import { getIDFromBase64 } from "@/_lib/relayHelpers";
