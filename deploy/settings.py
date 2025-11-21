1a941428c798f76f1696765487cec72aa14f0ae8
Ilia Peterov <ipeterov1@gmail.com>
2025-11-21 22:58:26 +0400

    Increase web worker count 4 -> 8
    I want to check whether it helps with web requests nginx queueing

--- deploy/settings.py
+++ deploy/settings.py
@@ -175,7 +175,7 @@ class CeleryWorkerTask(CeleryTask):
 SERVICES = [
     WebService(
         name="webService",
-        count=4,
+        count=8,
         task=WebTask(
             family="websiteTask",
             command="",
