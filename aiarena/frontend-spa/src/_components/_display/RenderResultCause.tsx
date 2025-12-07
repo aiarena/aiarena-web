e5b578c5ff51fe4151c552d4d5e415066411451b
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-12-07 14:11:06 +0100

    Add render result cause component

---
+++ aiarena/frontend-spa/src/_components/_display/RenderResultCause.tsx
@@ -0,0 +1,37 @@
+import { CoreMatchParticipationResultCauseChoices } from "@/_pages/Rework/Bot/__generated__/BotResultsTable_bot.graphql";
+import { resultCauseOptions } from "@/_pages/Rework/Bot/CustomOptions/ResultCauseOptions";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+export function RenderResultCause({
+  cause,
+}: {
+  cause: CoreMatchParticipationResultCauseChoices | null | undefined | "";
+}) {
+  if (cause === null || cause === undefined || cause === "") {
+    return "";
+  }
+
+  const readableResult = resultCauseOptions.find(
+    (opt) => opt.id === cause.toUpperCase()
+  );
+
+  if (
+    readableResult?.name === "Initialization Failure" ||
+    readableResult?.name === "Crash" ||
+    readableResult?.name === "Race Mismatch" ||
+    readableResult?.name === "Error"
+  ) {
+    return (
+      <>
+        <span className="flex gap-2 items-center text-amber-300 ">
+          <span>
+            <ExclamationTriangleIcon height={20} />
+          </span>
+          <span>{readableResult?.name}</span>
+        </span>
+      </>
+    );
+  }
+
+  return readableResult?.name ?? cause;
+}
