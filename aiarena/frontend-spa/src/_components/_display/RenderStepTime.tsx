b9c9ea641b7c1c255f89ca44851c4191a12c7c90
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-12-07 14:11:28 +0100

    Add render step time component

---
+++ aiarena/frontend-spa/src/_components/_display/RenderStepTime.tsx
@@ -0,0 +1,16 @@
+import getRedGreenDynamicColor from "@/_lib/getRedGreenDynamicColor";
+
+export default function StepTime({
+  time,
+}: {
+  time: string | number | null | undefined;
+}) {
+  const stepTime = Math.floor(Number(time) * 1000);
+  const color = getRedGreenDynamicColor({
+    value: stepTime,
+    rangeMin: 100,
+    rangeMax: 0,
+  });
+
+  return <span style={{ color }}>{stepTime} ms</span>;
+}
