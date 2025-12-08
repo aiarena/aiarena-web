a3a9b3f2d97dbf6dc6bf6feeb765d7cfcc0b6036
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-12-08 21:55:16 +0100

    add map and download link to match info

--- aiarena/frontend-spa/src/_pages/Rework/_Match/MatchDecal.tsx
+++ aiarena/frontend-spa/src/_pages/Rework/_Match/MatchDecal.tsx
@@ -3,7 +3,7 @@ import { MatchDecal_match$key } from "./__generated__/MatchDecal_match.graphql";
 import EloTrendIcon from "@/_components/_display/EloTrendIcon";
 import { getIDFromBase64 } from "@/_lib/relayHelpers";
 import { formatWinnerName } from "@/_components/_display/formatWinnerName";
-import { TrophyIcon } from "@heroicons/react/24/outline";
+import { ArrowDownCircleIcon, TrophyIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import RenderCodeLanguage from "@/_components/_display/RenderCodeLanguage";
 interface MatchDecalProps {
@@ -76,12 +76,12 @@ export default function MatchInfo(props: MatchDecalProps) {
 
   return (
     <div className="mb-8 rounded-2xl border border-neutral-800 bg-darken-2 p-4 sm:p-5 shadow-lg shadow-black">
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full  mb-6">
         <h2 id="match-heading" className="sr-only">
           Match {match.id}
         </h2>
 
-        <div className="items-baseline gap-2 mb-6">
+        <div className="items-baseline gap-2">
           <div className="flex">
             <h2
               id="bot-information-heading"
@@ -90,9 +90,18 @@ export default function MatchInfo(props: MatchDecalProps) {
               Match {getIDFromBase64(match.id, "MatchType")}
             </h2>
           </div>
+          <span className="ml-2 flex gap-2">
+            on{" "}
+            <a
+              href={`${match.map.downloadLink}`}
+              className="flex gap-1 items-center"
+            >
+              <ArrowDownCircleIcon height={18} /> {match.map.name}
+            </a>
+          </span>
         </div>
 
-        <div className="flex items-center justify-between gap-3 mb-3 flex-wrap">
+        <div className="flex items-center justify-between mb-3 flex-wrap">
           <div className="flex items-center gap-2">
             <div className="flex flex-col">
               <span className="text-xl font-semibold text-white">
@@ -139,14 +148,14 @@ export default function MatchInfo(props: MatchDecalProps) {
           </div>
 
           {/* VS Divider */}
-          <div className="flex flex-col items-center mx-2 my-1 text-xs uppercase tracking-wide text-gray-400">
+          <div className="flex flex-col items-center mx-2 my-1  text-gray-400">
             <div className="inline-flex items-center justify-center rounded-full bg-neutral-900/80 border border-customGreen p-1.5 shadow-black mb-2">
               <TrophyIcon
                 className="h-7 w-7 text-customGreen"
                 aria-hidden="true"
               />
             </div>
-            <span className="hidden md:inline">VS</span>
+            <span className="hidden md:inline ">VS</span>
             <span className="md:hidden">- VS -</span>
           </div>
 
