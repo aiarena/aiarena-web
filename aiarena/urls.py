089151c2aa8f2c135ab21af715f504d8fe52c59c
DrillableBit <a.a.p.lindberg69@gmail.com>
2025-12-14 13:46:54 +0100

    Release Author, Competitions, matches

--- aiarena/urls.py
+++ aiarena/urls.py
@@ -82,11 +82,11 @@ urlpatterns = [  # todo: replace usage of url with path for all these
     ),
     path("match-logs/<int:pk>/", core_views.MatchLogDownloadView.as_view()),
     path("authors/", core_views.frontend, name="authors"),
-    path("authors/<int:pk>/", core_views.AuthorDetail.as_view(), name="author"),
+    path("authors/<int:pk>/", core_views.frontend, name="author"),
     # path('rounds/', core_views.RoundList.as_view(), name='rounds'), # todo
     path("rounds/<int:pk>/", core_views.frontend, name="round"),
-    path("matches/<int:pk>/", core_views.MatchDetail.as_view(), name="match"),
-    path("competitions/", core_views.CompetitionList.as_view(), name="competitions"),
+    path("matches/<int:pk>/", core_views.frontend, name="match"),
+    path("competitions/", core_views.frontend, name="competitions"),
     path("competitions/<int:pk>/", core_views.frontend, name="competition"),
     path("competitions/stats/<int:pk>/", core_views.BotCompetitionStatsDetail.as_view()),
     path(
