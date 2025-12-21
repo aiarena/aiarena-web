b5690a8835d7c7ee7224e8377eebb2307582db37
lladdy <randallboland@bigpond.com>
2025-12-21 12:48:34 +1030

    Add ArenaClients service

--- aiarena/core/services/__init__.py
+++ aiarena/core/services/__init__.py
@@ -7,3 +7,19 @@ from .match_requests import MatchRequests
 from .matches import Matches
 from .supporter_benefits import SupporterBenefits
 from .users import Users
+
+from .arena_clients import ArenaClients as _ArenaClientsClass
+
+arena_clients = _ArenaClientsClass()
+
+__all__ = [
+    "BotStatistics",
+    "Bots",
+    "Competitions",
+    "Ladders",
+    "MatchRequests",
+    "Matches",
+    "SupporterBenefits",
+    "Users",
+    "arena_clients",
+]
