84379c4542211a9ac1520db92ce1964fdd9d0e58
lladdy <randallboland@bigpond.com>
2025-12-21 13:15:43 +1030

    Create universal instance of BotStatistics service.

--- aiarena/core/services/__init__.py
+++ aiarena/core/services/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa: F401
-from .bot_statistics import BotStatistics
+from .bot_statistics import BotStatistics as _BotStatisticsClass
 from .bots import Bots
 from .competitions import Competitions
 from .ladders import Ladders
@@ -11,9 +11,9 @@ from .users import Users
 from .arena_clients import ArenaClients as _ArenaClientsClass
 
 arena_clients = _ArenaClientsClass()
+bot_statistics = _BotStatisticsClass()
 
 __all__ = [
-    "BotStatistics",
     "Bots",
     "Competitions",
     "Ladders",
@@ -22,4 +22,5 @@ __all__ = [
     "SupporterBenefits",
     "Users",
     "arena_clients",
+    "bot_statistics",
 ]
