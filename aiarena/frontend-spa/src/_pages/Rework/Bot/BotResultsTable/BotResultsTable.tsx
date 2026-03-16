9d92553893f47c990cfeb9a55aae7636e329e59b
DrillableBit <a.a.p.lindberg69@gmail.com>
2026-03-16 22:07:03 +0100

    Add since last updated button

--- aiarena/frontend-spa/src/_pages/Rework/Bot/BotResultsTable/BotResultsTable.tsx
+++ aiarena/frontend-spa/src/_pages/Rework/Bot/BotResultsTable/BotResultsTable.tsx
@@ -7,7 +7,11 @@ import {
   VisibilityState,
 } from "@tanstack/react-table";
 
-import { getIDFromBase64, getNodes } from "@/_lib/relayHelpers";
+import {
+  getBase64FromID,
+  getIDFromBase64,
+  getNodes,
+} from "@/_lib/relayHelpers";
 import clsx from "clsx";
 
 import { useEffect, useMemo, useRef, useState } from "react";
@@ -38,8 +42,10 @@ import {
 } from "./__generated__/BotResultsTbody_bot.graphql";
 import { BotResultsTableSortingMap } from "./botResultTableSearchParams";
 import TagSummaryWithModal from "./TagSummaryModal";
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 import useStateWithLocalStorage from "@/_components/_hooks/useStateWithLocalStorage";
+import { graphql, useLazyLoadQuery } from "react-relay";
+import { BotResultsTableQuery } from "./__generated__/BotResultsTableQuery.graphql";
 
 interface BotResultsTableProps {
   data: BotResultsTbody_bot$key;
@@ -145,6 +151,21 @@ export type Tag = NonNullable<
 >["node"];
 
 export default function BotResultsTable(props: BotResultsTableProps) {
+  const { botId } = useParams<{ botId: string }>();
+
+  const data = useLazyLoadQuery<BotResultsTableQuery>(
+    graphql`
+      query BotResultsTableQuery($id: ID!) {
+        node(id: $id) {
+          ... on BotType {
+            botZipUpdated
+          }
+        }
+      }
+    `,
+    { id: getBase64FromID(botId!, "BotType") || "" },
+  );
+
   const [sorting, setSorting] = useState<SortingState>(
     () => props.initialSorting ?? [],
   );
@@ -566,6 +587,18 @@ export default function BotResultsTable(props: BotResultsTableProps) {
                 apply({ ...filters, includeFinished: !filters.includeFinished })
               }
               text="Finished"
+            />
+            <ButtonToggle
+              active={!!filters.matchStartedAfter}
+              onClick={() =>
+                apply({
+                  ...filters,
+                  matchStartedAfter: filters.matchStartedAfter
+                    ? undefined
+                    : data.node?.botZipUpdated,
+                })
+              }
+              text="Since Updated"
             />
           </div>
         }
