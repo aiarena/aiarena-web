f447ed5fb18a74b606c0a9cbda4b7f7db79e2fa3
DrillableBit <a.a.p.lindberg69@gmail.com>
2026-07-28 18:16:41 +0200

    return bot data state from mutation

--- aiarena/frontend-spa/src/_pages/UserBots/UserBotsSection/_modals/bot_settings_modal/BotSettingsModal.tsx
+++ aiarena/frontend-spa/src/_pages/UserBots/UserBotsSection/_modals/bot_settings_modal/BotSettingsModal.tsx
@@ -61,6 +61,7 @@ export default function BotSettingsModal({
             botDataPubliclyDownloadable
             botZipPubliclyDownloadable
             wikiArticle
+            botData
             botZip
           }
         }
