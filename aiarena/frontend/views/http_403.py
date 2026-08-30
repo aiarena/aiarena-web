75ba7b87492eb7fdc8ac58efda59be279960d6f4
DrillableBit <a.a.p.lindberg69@gmail.com>
2026-08-30 11:39:28 +0200

    ruff fix

--- aiarena/frontend/views/http_403.py
+++ aiarena/frontend/views/http_403.py
@@ -1,5 +1,6 @@
 from aiarena.frontend.views import frontend
 
+
 def react_403(request, exception):
     response = frontend(request)
     response.status_code = 403
--- aiarena/frontend/views/http_404.py
+++ aiarena/frontend/views/http_404.py
@@ -1,5 +1,6 @@
 from aiarena.frontend.views import frontend
 
+
 def react_404(request, exception):
     response = frontend(request)
     response.status_code = 404
