7cb9e48e738b3dc779d9401f2787b671a4872f7c
DrillableBit <a.a.p.lindberg69@gmail.com>
2026-09-06 13:41:36 +0200

    upgrade join competition modal to link

--- aiarena/frontend-spa/src/_pages/UserBots/UserBotsSection/_modals/JoinCompetitionModal.tsx
+++ aiarena/frontend-spa/src/_pages/UserBots/UserBotsSection/_modals/JoinCompetitionModal.tsx
@@ -12,6 +12,7 @@ import Modal from "@/_components/_actions/Modal";
 import { JoinCompetitionModalMutation } from "./__generated__/JoinCompetitionModalMutation.graphql";
 import SimpleToggle from "@/_components/_actions/_toggle/SimpleToggle";
 import useSnackbarErrorHandlers from "@/_lib/useSnackbarErrorHandlers";
+import { Link } from "react-router";
 
 interface JoinCompetitionModalProps {
   bot: JoinCompetitionModal_bot$key;
@@ -146,15 +147,15 @@ export default function JoinCompetitionModal({
             >
               <div className="block">
                 <div>
-                  <a
-                    href={reverseUrl("competition", {
+                  <Link
+                    to={reverseUrl("competition", {
                       pk: getIDFromBase64(comp.id, "CompetitionType"),
                     })}
                     target="_blank"
                     className="font-bold"
                   >
                     {comp.name}
-                  </a>
+                  </Link>
                   <p>{comp.status}</p>
                 </div>
               </div>
